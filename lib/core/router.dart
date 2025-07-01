0f4b53cf8a87c70422e586ad3e3656199f80a254
Riley-Mac <rileymitch117@gmail.com>
2025-07-01 14:53:13 +1000

    Add router configuration for account creation flow

---
+++ lib/core/router.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:swimming_app_frontend/features/app_start/ui/screens/splash_screen.dart';
+import 'package:swimming_app_frontend/features/auth/ui/screens/create_acc_add_name.dart';
+import 'package:swimming_app_frontend/features/auth/ui/screens/create_acc_add_dob.dart';
+
+final routerProvider = Provider<GoRouter>((ref) {
+  return GoRouter(
+    initialLocation: '/',
+    debugLogDiagnostics: true,
+    routes: [
+      GoRoute(
+        path: '/',
+        name: 'splash',
+        builder: (context, state) =>
+            const AppStartScreen(), // Replace with SplashScreen if
+      ),
+      GoRoute(
+        path: '/ca-initial',
+        name: 'initialCA',
+        builder: (context, state) => const AppStartScreen(),
+      ),
+      GoRoute(
+        path: '/ca-add-name',
+        name: 'addNameCA',
+        builder: (context, state) =>
+            const CreateAccAddName(), // Replace with AddNameScreen if exists
+      ),
+      GoRoute(
+        path: '/ca-add-dob',
+        name: 'addDOBCA',
+        builder: (context, state) =>
+            const CreateAccAddDOB(), // Replace with AddDOBScreen if exists
+      ),
+      //   GoRoute(
+      //     path: '/ca-add-height',
+      //     name: 'addHeightCA',
+      //     builder: (context, state) =>
+      //         const CreateAccAddHeight(), // Replace with AddHeightScreen
+      //   ),
+      //   GoRoute(
+      //     path: '/ca-add-sex',
+      //     name: 'addSexCA',
+      //     builder: (context, state) =>
+      //         const CreateAccAddSex(), // Replace with AddSexScreen
+      //   ),
+      //   GoRoute(
+      //     path: '/ca-add-phone-number',
+      //     name: 'addPhoneNumberCA',
+      //     builder: (context, state) =>
+      //         const CreateAccAddPhoneNum(), // Replace with AddPhoneNumberScreen
+      //   ),
+      //   GoRoute(
+      //     path: '/ca-verify-phone-number',
+      //     name: 'verifyPhoneNumberCA',
+      //     builder: (context, state) =>
+      //         const CreateAccVerifyPhoneNum(), // Replace with VerifyPhoneNumberScreen
+      //   ),
+      //   GoRoute(
+      //     path: '/ca-done',
+      //     name: 'doneCA',
+      //     builder: (context, state) =>
+      //         const CreateAccDone(), // Replace with DoneScreen
+      //   ),
+    ],
+  );
+});
