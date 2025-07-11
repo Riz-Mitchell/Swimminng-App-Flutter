d1eb907b3d5ecaa2996da558dd02eccd9c833d51
Riley-Mac <rileymitch117@gmail.com>
2025-07-11 13:32:56 +1000

    Small colour changes + early apiClient init

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:swimming_app_frontend/core/router.dart';
-import 'package:swimming_app_frontend/features/app_start/ui/screens/splash_screen.dart';
+import 'package:swimming_app_frontend/api/api.dart';
+import 'package:swimming_app_frontend/shared/router.dart';
+import 'package:swimming_app_frontend/features/app_start/presentation/screens/splash_screen.dart';
 
-void main() {
-  runApp(const ProviderScope(child: MyApp()));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final apiClient = ApiClient(baseUrl: 'https://api.inteliswim.com');
+  await apiClient.init();
+
+  runApp(
+    ProviderScope(
+      overrides: [apiClientProvider.overrideWithValue(apiClient)],
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends ConsumerWidget {
@@ -25,15 +36,17 @@ class MyApp extends ConsumerWidget {
           onPrimary: Color(0xFFFFFFFF),
           secondary: Color(0xFF999999),
           onSecondary: Color(0xFFFFFFFF),
-          background: Color(0xFFF5F5F5),
+          background: Color(0xFFEDEDED),
           onBackground: Color(0xFF212121),
-          surface: Color(0xFFFFFFFF),
+          surface: Color(0xFFFBFBFB),
           onSurface: Color(0xFF212121),
           error: Color(0xFFB00020),
           onError: Color(0xFFFFFFFF),
         ),
         textTheme: const TextTheme(
           displayLarge: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+          displayMedium: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          displaySmall: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
           bodyMedium: TextStyle(fontSize: 16, height: 1.5),
           labelSmall: TextStyle(fontSize: 12, letterSpacing: 1.2),
         ),
