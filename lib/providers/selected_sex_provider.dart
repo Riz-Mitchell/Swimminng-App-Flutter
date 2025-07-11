0dd0e5ef78aebed13a6d976f3e147e7538eef53a
Riley-Mac <rileymitch117@gmail.com>
2025-07-11 13:34:57 +1000

    Name changed, moved or restructured

--- lib/providers/selected_sex_provider.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:swimming_app_frontend/api/models/user_model.dart';
-
-final selectedSexProvider = StateProvider<Sex?>((ref) => null);
