e851acfb88bb9977b42d6ee25742e116b9f8f168
Riley-Mac <rileymitch117@gmail.com>
2025-07-24 12:09:50 +1000

    Refactor router provider to update screen imports and improve naming conventions for signup and login flows

--- lib/shared/application/providers/router_provider.dart
+++ lib/shared/application/providers/router_provider.dart
@@ -1,20 +1,19 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'package:swimming_app_frontend/features/auth/presentation/screens/login_done_screen.dart';
 import 'package:swimming_app_frontend/features/app_start/presentation/screens/onboard_app_start_screen.dart';
-import 'package:swimming_app_frontend/features/auth/presentation/screens/login_phone_num_screen.dart';
-import 'package:swimming_app_frontend/features/auth/presentation/screens/login_verify_screen.dart';
 import 'package:swimming_app_frontend/features/app_start/presentation/screens/launch_app_start_screen.dart';
-import 'package:swimming_app_frontend/features/auth/presentation/screens/add_height_screen.dart';
-import 'package:swimming_app_frontend/features/auth/presentation/screens/add_name_screen.dart';
-import 'package:swimming_app_frontend/features/auth/presentation/screens/add_dob_screen.dart';
-import 'package:swimming_app_frontend/features/auth/presentation/screens/add_phone_number_screen.dart';
-import 'package:swimming_app_frontend/features/auth/presentation/screens/add_sex_screen.dart';
-import 'package:swimming_app_frontend/features/auth/presentation/screens/verify_screen.dart';
-import 'package:swimming_app_frontend/features/auth/presentation/screens/initial_screen.dart';
 import 'package:swimming_app_frontend/features/home/presentation/screens/heart_rate_screen.dart';
 import 'package:swimming_app_frontend/features/home/presentation/screens/home.dart';
+import 'package:swimming_app_frontend/features/login/presentation/screens/done_login_screen.dart';
+import 'package:swimming_app_frontend/features/login/presentation/screens/phone_num_login_screen.dart';
+import 'package:swimming_app_frontend/features/login/presentation/screens/verify_login_screen.dart';
+import 'package:swimming_app_frontend/features/signup/presentation/screens/date_of_birth_signup_screen.dart';
+import 'package:swimming_app_frontend/features/signup/presentation/screens/initial_screen.dart';
+import 'package:swimming_app_frontend/features/signup/presentation/screens/name_signup_screen.dart';
+import 'package:swimming_app_frontend/features/signup/presentation/screens/phone_number_signup_screen.dart';
+import 'package:swimming_app_frontend/features/signup/presentation/screens/sex_signup_screen.dart';
+import 'package:swimming_app_frontend/features/signup/presentation/screens/verify_signup_screen.dart';
+import 'package:swimming_app_frontend/features/signup/presentation/widgets/height_picker_signup_widget.dart';
 import 'package:swimming_app_frontend/features/swims/presentation/screens/swims_landing_screen.dart';
 
 final routerProvider = Provider<GoRouter>((ref) {
@@ -26,7 +25,7 @@ final routerProvider = Provider<GoRouter>((ref) {
         path: '/',
         name: 'splash',
         builder: (context, state) =>
-            const AppStartScreen(), // Replace with SplashScreen if
+            const LaunchAppStartScreen(), // Replace with SplashScreen if
       ),
       GoRoute(
         path: '/ca-initial',
@@ -37,42 +36,42 @@ final routerProvider = Provider<GoRouter>((ref) {
         path: '/ca-add-name',
         name: 'addNameCA',
         builder: (context, state) =>
-            const CreateAccAddName(), // Replace with AddNameScreen if exists
+            const NameSignupScreen(), // Replace with AddNameScreen if exists
       ),
       GoRoute(
         path: '/ca-add-dob',
         name: 'addDOBCA',
         builder: (context, state) =>
-            const CreateAccAddDOB(), // Replace with AddDOBScreen if exists
+            const DateOfBirthSignupScreen(), // Replace with AddDOBScreen if exists
       ),
       GoRoute(
         path: '/ca-add-height',
         name: 'addHeightCA',
         builder: (context, state) =>
-            const CreateAccAddHeight(), // Replace with AddHeightScreen
+            const HeightPickerSignupWidget(), // Replace with AddHeightScreen
       ),
       GoRoute(
         path: '/ca-add-sex',
         name: 'addSexCA',
         builder: (context, state) =>
-            const CreateAccAddSex(), // Replace with AddSexScreen
+            const SexSignupScreen(), // Replace with AddSexScreen
       ),
       GoRoute(
         path: '/ca-add-phone-number',
         name: 'addPhoneNumberCA',
         builder: (context, state) =>
-            const CreateAccAddPhoneNumber(), // Replace with AddPhoneNumberScreen
+            const PhoneNumberSignupScreen(), // Replace with AddPhoneNumberScreen
       ),
       GoRoute(
         path: '/ca-verify-phone-number',
         name: 'verifyPhoneNumberCA',
         builder: (context, state) =>
-            const CreateAccVerify(), // Replace with VerifyPhoneNumberScreen
+            const VerifySignupScreen(), // Replace with VerifyPhoneNumberScreen
       ),
       GoRoute(
         path: '/login-or-signup',
         name: 'loginOrSignup',
-        builder: (context, state) => const LoginOrSignupScreen(),
+        builder: (context, state) => const OnboardAppStartScreen(),
       ),
       GoRoute(
         path: '/login-phonenumber',
