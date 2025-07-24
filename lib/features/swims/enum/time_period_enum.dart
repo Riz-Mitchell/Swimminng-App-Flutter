78b71234f93e45cf6207fb61ffdef4ca6fd4620d
Riley-Mac <rileymitch117@gmail.com>
2025-07-24 12:41:10 +1000

    Add 'day' option to TimePeriod enum and update date formatting logic

--- lib/features/swims/enum/time_period_enum.dart
+++ lib/features/swims/enum/time_period_enum.dart
@@ -1,6 +1,6 @@
 import 'package:intl/intl.dart';
 
-enum TimePeriod { week, month, threeMonths, sixMonths, year }
+enum TimePeriod { day, week, month, threeMonths, sixMonths, year }
 
 /// Returns a tuple of (startDate, endDate) based on selectedPeriod.
 List<DateTime> getTimeRange(TimePeriod period) {
@@ -9,6 +9,9 @@ List<DateTime> getTimeRange(TimePeriod period) {
   DateTime endDate = now;
 
   switch (period) {
+    case TimePeriod.day:
+      startDate = now.subtract(Duration(days: 1));
+      break;
     case TimePeriod.week:
       startDate = now.subtract(Duration(days: 7));
       break;
@@ -36,6 +39,14 @@ String formatDate(DateTime date) {
 class DateLabelConverter {
   static String format(DateTime date, TimePeriod timePeriod) {
     switch (timePeriod) {
+      case TimePeriod.day:
+        final now = DateTime.now();
+        final isYesterday =
+            date.day == now.subtract(Duration(days: 1)).day &&
+            date.month == now.month &&
+            date.year == now.year;
+        final timeString = DateFormat('h:mma').format(date);
+        return isYesterday ? 'Yesterday, $timeString' : timeString;
       case TimePeriod.week:
         // Example: Monday, Tuesday
         return DateFormat('h:mma EEE').format(date);
