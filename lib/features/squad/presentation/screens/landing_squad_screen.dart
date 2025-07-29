26abd0766d5035b34a46b7ac5ba0fe68f1e7cf3b
Riley-Mac <rileymitch117@gmail.com>
2025-07-30 09:31:25 +1000

    Update LandingSquadScreen to include BasicCardSquadWidget, LeaderboardSummarySquadWidget, and SessionSummarySquadWidget

--- lib/features/squad/presentation/screens/landing_squad_screen.dart
+++ lib/features/squad/presentation/screens/landing_squad_screen.dart
@@ -1,15 +1,27 @@
+import 'package:easy_date_timeline/easy_date_timeline.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:swimming_app_frontend/features/squad/presentation/widgets/basic_card_squad_widget.dart';
+import 'package:swimming_app_frontend/features/squad/presentation/widgets/leaderboard_squad_wdiget.dart';
+import 'package:swimming_app_frontend/features/squad/presentation/widgets/leaderboard_summary_squad_widget.dart';
+import 'package:swimming_app_frontend/features/squad/presentation/widgets/session_summary_squad_widget.dart';
 
 class LandingSquadScreen extends ConsumerWidget {
   const LandingSquadScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final textTheme = Theme.of(context).textTheme;
+    final colorScheme = Theme.of(context).colorScheme;
+
     return Column(
-      spacing: 200,
+      spacing: 50,
       mainAxisAlignment: MainAxisAlignment.start,
-      children: [],
+      children: [
+        BasicCardSquadWidget(),
+        LeaderboardSummarySquadWidget(),
+        SessionSummarySquadWidget(),
+      ],
     );
   }
 }
