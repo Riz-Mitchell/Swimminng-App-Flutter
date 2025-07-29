b97b6dc9bcc9e55d9a3ef3d14daa129efd08d8c3
Riley-Mac <rileymitch117@gmail.com>
2025-07-29 11:58:48 +1000

    Refactor LandingLogbookScreen to use a Column layout and update logbook widgets

--- lib/features/logbook/presentation/screens/landing_logbook_screen.dart
+++ lib/features/logbook/presentation/screens/landing_logbook_screen.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:swimming_app_frontend/features/logbook/presentation/widgets/data_logbook_widget.dart';
+import 'package:swimming_app_frontend/features/logbook/presentation/widgets/calendar_logbook_widget.dart';
+import 'package:swimming_app_frontend/features/logbook/presentation/widgets/carousel_logbook_widget.dart';
+import 'package:swimming_app_frontend/features/logbook/presentation/widgets/data_container_logbook_widget.dart';
+import 'package:swimming_app_frontend/shared/presentation/widgets/primary_button_widget.dart';
 
 class LandingLogbookScreen extends ConsumerWidget {
   const LandingLogbookScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Wrap(
-      runSpacing: 20,
-      spacing: 20,
+    return Column(
+      spacing: 200,
+      mainAxisAlignment: MainAxisAlignment.start,
       children: [
-        DataLogbookWidget(data: '72', text: 'Swims'),
-        DataLogbookWidget(data: '5', text: 'Workouts'),
+        CarouselLogbookWidget(),
+        DataContainerLogbookWidget(),
+        CalendarLogbookWidget(),
+        PrimaryButtonWidget(text: 'Add Swim'),
       ],
     );
   }
