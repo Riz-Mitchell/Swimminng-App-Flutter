3532df87f610fc5fe9a3f4f4996a1f47d6428ef6
Riley-Mac <rileymitch117@gmail.com>
2025-07-29 11:59:27 +1000

    Enhance DataLogbookWidget with SVG asset support and improved layout

--- lib/features/logbook/presentation/widgets/data_logbook_widget.dart
+++ lib/features/logbook/presentation/widgets/data_logbook_widget.dart
@@ -1,34 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class DataLogbookWidget extends ConsumerWidget {
   final dynamic data;
   final String text;
+  final String asset;
 
-  const DataLogbookWidget({super.key, required this.data, required this.text});
+  const DataLogbookWidget({
+    super.key,
+    required this.data,
+    required this.text,
+    required this.asset,
+  });
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final colorScheme = Theme.of(context).colorScheme;
     final textTheme = Theme.of(context).textTheme;
 
-    return SizedBox(
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+    return Container(
+      padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20, top: 20),
       decoration: BoxDecoration(
         color: colorScheme.surface,
-        borderRadius: BorderRadius.circular(8),
+        borderRadius: BorderRadius.circular(25),
       ),
       child: Column(
-        spacing: 1,
+        spacing: 30,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
-            data.toString(),
-            style: textTheme.displayLarge?.copyWith(color: colorScheme.primary),
+          Align(
+            alignment: Alignment.topRight,
+            child: SvgPicture.asset(
+              asset,
+              width: 20,
+              height: 20,
+              colorFilter: ColorFilter.mode(
+                colorScheme.primary,
+                BlendMode.srcIn,
+              ),
+            ),
           ),
-          Text(
-            text,
-            style: textTheme.bodyMedium?.copyWith(color: colorScheme.secondary),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end, // Pushes to bottom
+            crossAxisAlignment: CrossAxisAlignment.start, // Aligns left
+            mainAxisSize: MainAxisSize.max, // Fills vertical space
+            children: [
+              Text(
+                data.toString(),
+                style: textTheme.displayMedium?.copyWith(
+                  color: colorScheme.primary,
+                ),
+              ),
+              Text(
+                text,
+                style: textTheme.bodyMedium?.copyWith(
+                  color: colorScheme.secondary,
+                ),
+              ),
+            ],
           ),
         ],
       ),
