8c2aafed0232c9d2144efca2d295228f393655c8
Riley-Mac <rileymitch117@gmail.com>
2025-07-30 09:31:13 +1000

    Add LeaderboardSummaryItemSquadWidget for displaying leaderboard items

---
+++ lib/features/squad/presentation/widgets/leaderboard_summary_item_squad_widget.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class LeaderboardSummaryItemSquadWidget extends ConsumerWidget {
+  final String leaderboardName;
+  final String description;
+  final String asset;
+
+  const LeaderboardSummaryItemSquadWidget({
+    Key? key,
+    required this.leaderboardName,
+    required this.description,
+    required this.asset,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final colorScheme = Theme.of(context).colorScheme;
+    final textTheme = Theme.of(context).textTheme;
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return Row(
+      spacing: 20,
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        SvgPicture.asset(
+          asset,
+          width: 40,
+          height: 40,
+          colorFilter: ColorFilter.mode(colorScheme.primary, BlendMode.srcIn),
+        ),
+        Expanded(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                leaderboardName,
+                style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+              ),
+              Text(
+                description,
+                style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                  color: Theme.of(context).colorScheme.secondary,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
