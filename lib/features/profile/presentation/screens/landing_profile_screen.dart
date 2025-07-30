930e2cc081e9829b58fbd870b0a8d89180e78055
Riley-Mac <rileymitch117@gmail.com>
2025-07-30 13:37:15 +1000

    Refactor LandingProfileScreen to improve layout and integrate profile widgets

--- lib/features/profile/presentation/screens/landing_profile_screen.dart
+++ lib/features/profile/presentation/screens/landing_profile_screen.dart
@@ -1,29 +1,32 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:swimming_app_frontend/features/profile/presentation/widgets/carousel_profile_widget.dart';
+import 'package:swimming_app_frontend/features/profile/presentation/widgets/header_profile_widget.dart';
+import 'package:swimming_app_frontend/features/profile/presentation/widgets/stroke_distribution_radar_profile_widget.dart';
+import 'package:swimming_app_frontend/shared/presentation/theme/metric_colors.dart';
 
 class LandingProfileScreen extends ConsumerWidget {
-  const LandingProfileScreen({Key? key}) : super(key: key);
+  const LandingProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final colorScheme = Theme.of(context).colorScheme;
+    final textTheme = Theme.of(context).textTheme;
+    final screenWidth = MediaQuery.of(context).size.width;
+
     return Column(
       spacing: 50,
-      mainAxisAlignment: MainAxisAlignment.start,
       children: [
-        Container(
-          padding: const EdgeInsets.all(20),
-          decoration: BoxDecoration(
-            color: Theme.of(context).colorScheme.surface,
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(20),
-              bottomRight: Radius.circular(20),
-            ),
-          ),
-          child: Text(
-            'Profile Information',
-            style: Theme.of(context).textTheme.headlineMedium,
-          ),
+        HeaderProfileWidget(
+          name: 'Rizzle',
+          swims: 1450,
+          joinedDate: DateTime(2025, 1, 1),
+          friends: 231,
         ),
+        StrokeDistributionRadarProfileWidget(),
+        CarouselProfileWidget(),
       ],
     );
   }
