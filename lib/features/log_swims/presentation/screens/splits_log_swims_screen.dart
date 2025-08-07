ab8f9c96c5549df342a06112111cf0f0281850bb
Riley-Mac <rileymitch117@gmail.com>
2025-08-07 12:46:17 +1000

    Add Next button to SplitsLogSwimsScreen for navigation

--- lib/features/log_swims/presentation/screens/splits_log_swims_screen.dart
+++ lib/features/log_swims/presentation/screens/splits_log_swims_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:swimming_app_frontend/features/log_swims/application/log_swim_provider.dart';
 import 'package:swimming_app_frontend/features/log_swims/presentation/screens/log_swims_shell_screen.dart';
 import 'package:swimming_app_frontend/features/log_swims/presentation/widgets/header_log_swims_widget.dart';
+import 'package:swimming_app_frontend/shared/presentation/widgets/metric_button_widget.dart';
 
 class SplitsLogSwimsScreen extends ConsumerWidget {
   const SplitsLogSwimsScreen({super.key});
@@ -23,6 +25,14 @@ class SplitsLogSwimsScreen extends ConsumerWidget {
             style: textTheme.displayMedium?.copyWith(
               color: colorScheme.primary,
             ),
+          ),
+          MetricButtonWidget(
+            text: 'Next',
+            onPressed: () {
+              if (true) {
+                ref.read(logSwimProvider.notifier).navigateToNextStep();
+              }
+            },
           ),
         ],
       ),
