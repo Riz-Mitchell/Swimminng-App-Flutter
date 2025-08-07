4e072be90fe8ec8decedbc538ab15c32bb16b2ea
Riley-Mac <rileymitch117@gmail.com>
2025-08-07 12:46:48 +1000

    Refactor StrokeButtonLogSwimsWidget to manage stroke selection state and update UI with SVG icons

--- lib/features/log_swims/presentation/widgets/stroke_button_log_swims_widget.dart
+++ lib/features/log_swims/presentation/widgets/stroke_button_log_swims_widget.dart
@@ -1,45 +1,82 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:swimming_app_frontend/features/log_swims/application/selected_event_stroke_log_swims_provider.dart';
 import 'package:swimming_app_frontend/features/log_swims/domain/enum/stroke_enum.dart';
+import 'package:swimming_app_frontend/shared/presentation/theme/metric_colors.dart';
 
 class StrokeButtonLogSwimsWidget extends ConsumerWidget {
-  final VoidCallback? onTap;
   final StrokeEnum stroke;
+  final bool isSelected;
 
   const StrokeButtonLogSwimsWidget({
     super.key,
-    this.onTap,
     required this.stroke,
+    this.isSelected = false,
   });
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final colorScheme = Theme.of(context).colorScheme;
+    final currColor = isSelected ? metricBlue : colorScheme.secondary;
+
+    const widthAndHeight = 75.0;
+
     return GestureDetector(
-      onTap: () => _handleOnTap(),
+      onTap: () => _handleOnTap(ref),
       child: AnimatedContainer(
         duration: const Duration(milliseconds: 300),
         padding: const EdgeInsets.all(20),
         decoration: BoxDecoration(
-          color: Theme.of(context).colorScheme.primary,
-          borderRadius: BorderRadius.circular(8),
-          border: Border.all(
-            color: Theme.of(context).colorScheme.onPrimary,
-            width: 2,
-          ),
+          color: Colors.transparent,
+          borderRadius: BorderRadius.circular(20),
+          border: Border.all(color: currColor, width: 2),
         ),
-        child: Text(
-          stroke.name,
-          style: Theme.of(context).textTheme.labelLarge?.copyWith(
-            color: Theme.of(context).colorScheme.onPrimary,
+        child: AnimatedSwitcher(
+          duration: const Duration(milliseconds: 300),
+          child: SvgPicture.asset(
+            _getSvgPath(),
+            key: ValueKey<bool>(isSelected),
+            width: widthAndHeight,
+            height: widthAndHeight,
+            colorFilter: ColorFilter.mode(currColor, BlendMode.srcIn),
           ),
         ),
       ),
     );
   }
 
-  void _handleOnTap() {
-    if (onTap != null) {
-      onTap!.call();
+  void _handleOnTap(WidgetRef ref) {
+    final selectedEventStrokeNotifier = ref.read(
+      selectedEventStrokeLogSwimsProvider.notifier,
+    );
+
+    switch (stroke) {
+      case StrokeEnum.freestyle:
+        selectedEventStrokeNotifier.handleTappedStroke(StrokeEnum.freestyle);
+        break;
+      case StrokeEnum.backstroke:
+        selectedEventStrokeNotifier.handleTappedStroke(StrokeEnum.backstroke);
+        break;
+      case StrokeEnum.breaststroke:
+        selectedEventStrokeNotifier.handleTappedStroke(StrokeEnum.breaststroke);
+        break;
+      case StrokeEnum.butterfly:
+        selectedEventStrokeNotifier.handleTappedStroke(StrokeEnum.butterfly);
+        break;
+    }
+  }
+
+  String _getSvgPath() {
+    switch (stroke) {
+      case StrokeEnum.freestyle:
+        return 'assets/svg/Freestyle.svg';
+      case StrokeEnum.backstroke:
+        return 'assets/svg/Backstroke.svg';
+      case StrokeEnum.breaststroke:
+        return 'assets/svg/Breaststroke.svg';
+      case StrokeEnum.butterfly:
+        return 'assets/svg/Butterfly.svg';
     }
   }
 }
