adbfed984d5a457e4297f16e2199f3e1e3f81526
Riley-Mac <rileymitch117@gmail.com>
2025-08-07 12:46:21 +1000

    Add StrokeSelector and Next button to StrokeLogSwimsScreen for navigation

--- lib/features/log_swims/presentation/screens/stroke_log_swims_screen.dart
+++ lib/features/log_swims/presentation/screens/stroke_log_swims_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:swimming_app_frontend/features/log_swims/application/log_swim_provider.dart';
+import 'package:swimming_app_frontend/features/log_swims/application/selected_event_stroke_log_swims_provider.dart';
 import 'package:swimming_app_frontend/features/log_swims/presentation/screens/log_swims_shell_screen.dart';
 import 'package:swimming_app_frontend/features/log_swims/presentation/widgets/header_log_swims_widget.dart';
+import 'package:swimming_app_frontend/features/log_swims/presentation/widgets/stroke_selector_log_swims_widget.dart';
+import 'package:swimming_app_frontend/shared/presentation/widgets/metric_button_widget.dart';
 
 class StrokeLogSwimsScreen extends ConsumerWidget {
   const StrokeLogSwimsScreen({super.key});
@@ -10,6 +14,9 @@ class StrokeLogSwimsScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final textTheme = Theme.of(context).textTheme;
     final colorScheme = Theme.of(context).colorScheme;
+
+    // Watch to trigger rebuilds
+    final selectedEventStroke = ref.watch(selectedEventStrokeLogSwimsProvider);
 
     return LogSwimsShellScreen(
       child: Column(
@@ -23,6 +30,18 @@ class StrokeLogSwimsScreen extends ConsumerWidget {
             style: textTheme.displayMedium?.copyWith(
               color: colorScheme.primary,
             ),
+          ),
+          StrokeSelectorLogSwimsWidget(),
+          MetricButtonWidget(
+            isEnabled: ref
+                .read(selectedEventStrokeLogSwimsProvider.notifier)
+                .isValidSelection(),
+            text: 'Next',
+            onPressed: () {
+              if (true) {
+                ref.read(logSwimProvider.notifier).navigateToNextStep();
+              }
+            },
           ),
         ],
       ),
