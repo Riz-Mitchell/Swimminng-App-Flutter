537e864e92055aaafee47246b61c1df26fb04ccb
Riley-Mac <rileymitch117@gmail.com>
2025-08-07 14:50:17 +1000

    Refactor PoolTypeSelectorLogSwimsWidget to use Wrap layout and manage selected pool type state

--- lib/features/log_swims/presentation/widgets/pool_type_selector_log_swims_widget.dart
+++ lib/features/log_swims/presentation/widgets/pool_type_selector_log_swims_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:swimming_app_frontend/features/log_swims/application/pool_type_log_swims_provider.dart';
 import 'package:swimming_app_frontend/features/log_swims/domain/enum/selected_pool_type_enum.dart';
 import 'package:swimming_app_frontend/features/log_swims/presentation/widgets/pool_type_button_log_swims_widget.dart';
 
@@ -8,18 +9,29 @@ class PoolTypeSelectorLogSwimsWidget extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Column(
-      spacing: 20,
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    final selectedPoolTypeState = ref.watch(selectedPoolTypeProvider);
+    final selectedPoolTypeNotifier = ref.read(
+      selectedPoolTypeProvider.notifier,
+    );
+
+    return Wrap(
+      spacing: 10,
+      runSpacing: 10,
       children: [
         PoolTypeButtonLogSwimsWidget(
           type: SelectedPoolTypeEnum.longCourseMeters,
+          isSelected:
+              selectedPoolTypeState == SelectedPoolTypeEnum.longCourseMeters,
         ),
         PoolTypeButtonLogSwimsWidget(
           type: SelectedPoolTypeEnum.shortCourseMeters,
+          isSelected:
+              selectedPoolTypeState == SelectedPoolTypeEnum.shortCourseMeters,
         ),
         PoolTypeButtonLogSwimsWidget(
           type: SelectedPoolTypeEnum.shortCourseYards,
+          isSelected:
+              selectedPoolTypeState == SelectedPoolTypeEnum.shortCourseYards,
         ),
       ],
     );
