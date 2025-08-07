2a42b045503f28ffffe5b75ab0fea6bfdeed91f5
Riley-Mac <rileymitch117@gmail.com>
2025-08-07 12:46:03 +1000

    Add navigation button to CompleteLogSwimsScreen for step transitions

--- lib/features/log_swims/presentation/screens/complete_log_swims_screen.dart
+++ lib/features/log_swims/presentation/screens/complete_log_swims_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:swimming_app_frontend/features/log_swims/application/log_swim_provider.dart';
 import 'package:swimming_app_frontend/features/log_swims/presentation/screens/log_swims_shell_screen.dart';
 import 'package:swimming_app_frontend/features/log_swims/presentation/widgets/header_log_swims_widget.dart';
+import 'package:swimming_app_frontend/shared/presentation/widgets/metric_button_widget.dart';
 
 class CompleteLogSwimsScreen extends ConsumerWidget {
   const CompleteLogSwimsScreen({super.key});
@@ -17,12 +19,20 @@ class CompleteLogSwimsScreen extends ConsumerWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          HeaderLogSwimsWidget(),
+          HeaderLogSwimsWidget(donePage: true),
           Text(
             'All Done!',
             style: textTheme.displayMedium?.copyWith(
               color: colorScheme.primary,
             ),
+          ),
+          MetricButtonWidget(
+            text: 'Complete',
+            onPressed: () {
+              if (true) {
+                ref.read(logSwimProvider.notifier).navigateToNextStep();
+              }
+            },
           ),
         ],
       ),
