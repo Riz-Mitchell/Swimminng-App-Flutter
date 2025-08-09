5ada3e44669a338acb94acf08a75e3652b024714
Riley-Mac <rileymitch117@gmail.com>
2025-08-09 12:10:14 +1000

    Refactor QuestionnaireButtonLogSwimsWidget to enhance UI layout and improve option selection handling

--- lib/features/log_swims/presentation/widgets/questionnaire_button_log_swims_widget.dart
+++ lib/features/log_swims/presentation/widgets/questionnaire_button_log_swims_widget.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:swimming_app_frontend/features/log_swims/application/post_swim_questionnaire_log_swims_provider.dart';
 import 'package:swimming_app_frontend/features/log_swims/domain/enum/questionnaire_options_enum.dart';
+import 'package:swimming_app_frontend/features/log_swims/domain/helpers/questionnaire_options_helper.dart';
 import 'package:swimming_app_frontend/shared/presentation/theme/metric_colors.dart';
 
 class QuestionnaireButtonLogSwimsWidget<T extends Enum> extends ConsumerWidget {
@@ -17,37 +19,58 @@ class QuestionnaireButtonLogSwimsWidget<T extends Enum> extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final colorScheme = Theme.of(context).colorScheme;
-    final currColor = isSelected ? metricBlue : colorScheme.secondary;
+    final textTheme = Theme.of(context).textTheme;
 
-    print(option.toString());
-
-    const widthAndHeight = 75.0;
+    const widthAndHeight = 60.0;
 
     return GestureDetector(
       onTap: () => _handleOnTap(ref),
-      child: AnimatedContainer(
-        duration: const Duration(milliseconds: 300),
-        padding: const EdgeInsets.all(20),
-        decoration: BoxDecoration(
-          color: Colors.transparent,
-          borderRadius: BorderRadius.circular(20),
-          border: Border.all(color: currColor, width: 2),
-        ),
-        child: AnimatedSwitcher(
-          duration: const Duration(milliseconds: 300),
-          child: SvgPicture.asset(
-            _getSvgPath(),
-            key: ValueKey<bool>(isSelected),
-            width: widthAndHeight,
-            height: widthAndHeight,
-            colorFilter: ColorFilter.mode(currColor, BlendMode.srcIn),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        spacing: 10,
+        children: [
+          AnimatedContainer(
+            duration: const Duration(milliseconds: 300),
+            padding: const EdgeInsets.all(15),
+            decoration: BoxDecoration(
+              color: getBackGroundColor(option, isSelected, colorScheme),
+              borderRadius: BorderRadius.circular(20),
+              border: Border.all(color: getBorderColor(option), width: 4),
+            ),
+            child: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 300),
+              child: SvgPicture.asset(
+                _getSvgPath(),
+                key: ValueKey<bool>(isSelected),
+                width: widthAndHeight,
+                height: widthAndHeight,
+                colorFilter: ColorFilter.mode(
+                  getSvgColor(option, isSelected, colorScheme),
+                  BlendMode.srcIn,
+                ),
+              ),
+            ),
           ),
-        ),
+          SizedBox(
+            width: 100,
+            child: Text(
+              textAlign: TextAlign.center,
+              getSubtitle(option),
+              style: textTheme.bodyMedium?.copyWith(color: colorScheme.primary),
+            ),
+          ),
+        ],
       ),
     );
   }
 
-  void _handleOnTap(WidgetRef ref) {}
+  void _handleOnTap(WidgetRef ref) {
+    print('Tapped option: $option, type: ${option.runtimeType}');
+    ref
+        .read(postSwimQuestionnaireLogSwimsProvider.notifier)
+        .handleSelectedOption(option);
+  }
 
   String _getSvgPath() {
     if (option is SelfTalkOptionsEnum) {
