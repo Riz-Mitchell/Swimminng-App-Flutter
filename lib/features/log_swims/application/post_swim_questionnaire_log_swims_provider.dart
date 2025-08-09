0420d14ba4b4168fea019150d2c2f7c0eea14e93
Riley-Mac <rileymitch117@gmail.com>
2025-08-09 12:09:37 +1000

    Refactor questionnaire option selection methods and add retrieval functions for available options

--- lib/features/log_swims/application/post_swim_questionnaire_log_swims_provider.dart
+++ lib/features/log_swims/application/post_swim_questionnaire_log_swims_provider.dart
@@ -11,44 +11,142 @@ class PostSwimQuestionnaireLogSwimsProvider
     state = PostSwimQuestionnaireModel();
   }
 
-  void updateSelfTalk(SelfTalkOptionsEnum selfTalk) {
-    state = state.copyWith(selfTalk: selfTalk);
+  void selectSelfTalk(SelfTalkOptionsEnum option) {
+    print(
+      'inside selectSelfTalk with option: $option, runtimeType: ${option.runtimeType}',
+    );
+    state = state.updateQuestionnaireField(option, state.selfTalk);
   }
 
-  void updateNerves(NervesOptionsEnum nerves) {
-    state = state.copyWith(nerves: nerves);
+  void selectNerve(NervesOptionsEnum option) {
+    state = state.updateQuestionnaireField(option, state.nerves);
   }
 
-  void updateEnergyLevel(EnergyLevelOptionsEnum energyLevel) {
-    state = state.copyWith(energyLevel: energyLevel);
+  void selectEnergyLevel(EnergyLevelOptionsEnum option) {
+    state = state.updateQuestionnaireField(option, state.energyLevel);
   }
 
-  void updateBreathing(BreathingOptionsEnum breathing) {
-    state = state.copyWith(breathing: breathing);
+  void selectBreathing(BreathingOptionsEnum option) {
+    state = state.updateQuestionnaireField(option, state.breathing);
   }
 
-  void updateCatchFeel(CatchFeelOptionsEnum catchFeel) {
-    state = state.copyWith(catchFeel: catchFeel);
+  void selectCatchFeel(CatchFeelOptionsEnum option) {
+    state = state.updateQuestionnaireField(option, state.catchFeel);
   }
 
-  void updateStrokeLength(StrokeLengthOptionsEnum strokeLength) {
-    state = state.copyWith(strokeLength: strokeLength);
+  void selectStrokeLength(StrokeLengthOptionsEnum option) {
+    state = state.updateQuestionnaireField(option, state.strokeLength);
   }
 
-  void updateKickTechnique(KickTechniqueOptionsEnum kickTechnique) {
-    state = state.copyWith(kickTechnique: kickTechnique);
+  void selectKickTechnique(KickTechniqueOptionsEnum option) {
+    state = state.updateQuestionnaireField(option, state.kickTechnique);
   }
 
-  void updateKickThroughout(KickThroughoutOptionsEnum kickThroughout) {
-    state = state.copyWith(kickThroughout: kickThroughout);
+  void selectKickThroughout(KickThroughoutOptionsEnum option) {
+    state = state.updateQuestionnaireField(option, state.kickThroughout);
   }
 
-  void updateHeadPosition(HeadPositionOptionsEnum headPosition) {
-    state = state.copyWith(headPosition: headPosition);
+  void selectHeadPosition(HeadPositionOptionsEnum option) {
+    state = state.updateQuestionnaireField(option, state.headPosition);
   }
 
-  void updateTurn(TurnOptionsEnum turn) {
-    state = state.copyWith(turn: turn);
+  void selectTurn(TurnOptionsEnum option) {
+    state = state.updateQuestionnaireField(option, state.turn);
+  }
+
+  List<SelfTalkOptionsEnum> getAvailableSelfTalkOptions() {
+    final options = SelfTalkOptionsEnum.values
+        .where((option) => option != SelfTalkOptionsEnum.unselected)
+        .toList();
+
+    print('Available SelfTalk Options: ${options.length}');
+    return options;
+  }
+
+  List<NervesOptionsEnum> getAvailableNervesOptions() {
+    return NervesOptionsEnum.values
+        .where((option) => option != NervesOptionsEnum.unselected)
+        .toList();
+  }
+
+  List<EnergyLevelOptionsEnum> getAvailableEnergyLevelOptions() {
+    return EnergyLevelOptionsEnum.values
+        .where((option) => option != EnergyLevelOptionsEnum.unselected)
+        .toList();
+  }
+
+  List<BreathingOptionsEnum> getAvailableBreathingOptions() {
+    return BreathingOptionsEnum.values
+        .where((option) => option != BreathingOptionsEnum.unselected)
+        .toList();
+  }
+
+  List<CatchFeelOptionsEnum> getAvailableCatchFeelOptions() {
+    return CatchFeelOptionsEnum.values
+        .where((option) => option != CatchFeelOptionsEnum.unselected)
+        .toList();
+  }
+
+  List<StrokeLengthOptionsEnum> getAvailableStrokeLengthOptions() {
+    return StrokeLengthOptionsEnum.values
+        .where((option) => option != StrokeLengthOptionsEnum.unselected)
+        .toList();
+  }
+
+  List<KickTechniqueOptionsEnum> getAvailableKickTechniqueOptions() {
+    return KickTechniqueOptionsEnum.values
+        .where((option) => option != KickTechniqueOptionsEnum.unselected)
+        .toList();
+  }
+
+  List<KickThroughoutOptionsEnum> getAvailableKickThroughoutOptions() {
+    return KickThroughoutOptionsEnum.values
+        .where((option) => option != KickThroughoutOptionsEnum.unselected)
+        .toList();
+  }
+
+  List<HeadPositionOptionsEnum> getAvailableHeadPositionOptions() {
+    return HeadPositionOptionsEnum.values
+        .where((option) => option != HeadPositionOptionsEnum.unselected)
+        .toList();
+  }
+
+  List<TurnOptionsEnum> getAvailableTurnOptions() {
+    return TurnOptionsEnum.values
+        .where((option) => option != TurnOptionsEnum.unselected)
+        .toList();
+  }
+
+  void handleSelectedOption(Enum option) {
+    print(
+      'Handling selected option: $option, runtimeType: ${option.runtimeType}',
+    );
+    if (option is SelfTalkOptionsEnum) {
+      print('Selecting self talk option: $option');
+      selectSelfTalk(option);
+    } else if (option is NervesOptionsEnum) {
+      selectNerve(option);
+    } else if (option is EnergyLevelOptionsEnum) {
+      selectEnergyLevel(option);
+    } else if (option is BreathingOptionsEnum) {
+      selectBreathing(option);
+    } else if (option is CatchFeelOptionsEnum) {
+      selectCatchFeel(option);
+    } else if (option is StrokeLengthOptionsEnum) {
+      selectStrokeLength(option);
+    } else if (option is KickTechniqueOptionsEnum) {
+      selectKickTechnique(option);
+    } else if (option is KickThroughoutOptionsEnum) {
+      selectKickThroughout(option);
+    } else if (option is HeadPositionOptionsEnum) {
+      selectHeadPosition(option);
+    } else if (option is TurnOptionsEnum) {
+      selectTurn(option);
+    } else {
+      throw ArgumentError(
+        'Unknown option type in handleSelectedOption: $option',
+      );
+    }
   }
 }
 
