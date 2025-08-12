69c37c8f7e02773ccf7ba6fae09455a56df0fbf7
Riley-Mac <rileymitch117@gmail.com>
2025-08-12 10:56:46 +1000

    Update splitTimeLogSwimsProvider initial state format to '0000'

--- lib/features/log_swims/application/split_time_log_swims_provider.dart
+++ lib/features/log_swims/application/split_time_log_swims_provider.dart
@@ -1,3 +1,3 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final splitTimeLogSwimsProvider = StateProvider<String>((ref) => '00.00');
+final splitTimeLogSwimsProvider = StateProvider<String>((ref) => '0000');
