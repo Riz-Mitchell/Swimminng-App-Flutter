93d2e6309f3ec2f3bea396e20160e3850ee0cb90
Riley-Mac <rileymitch117@gmail.com>
2025-08-12 10:57:58 +1000

    Add StrokeRateInputLogSwimsWidget for stroke rate input functionality

---
+++ lib/features/log_swims/presentation/widgets/splits/stroke_rate_input_log_swims_widget.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:swimming_app_frontend/shared/presentation/theme/metric_colors.dart';
+
+final strokeRateProvider = StateProvider<int>((ref) => 0);
+
+class StrokeRateInputLogSwimsWidget extends ConsumerWidget {
+  const StrokeRateInputLogSwimsWidget({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final textTheme = Theme.of(context).textTheme;
+    final colorScheme = Theme.of(context).colorScheme;
+
+    final strokeRate = ref.watch(strokeRateProvider);
+
+    final strokeRateText = formatTime(strokeRate);
+
+    return Row(
+      textBaseline: TextBaseline.alphabetic,
+      spacing: 5,
+      crossAxisAlignment: CrossAxisAlignment.baseline,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+          width: 80, // fixed width to fit input nicely
+          child: TextField(
+            cursorColor: metricBlue,
+            textAlign: TextAlign.right,
+            textDirection: TextDirection.ltr,
+
+            controller: TextEditingController(text: strokeRateText)
+              ..selection = TextSelection.collapsed(
+                offset: strokeRateText.length,
+              ),
+            keyboardType: const TextInputType.numberWithOptions(),
+            inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+            maxLength: 2,
+            style: textTheme.displayLarge?.copyWith(
+              color: colorScheme.onPrimary,
+            ),
+            decoration: InputDecoration(
+              counterText: '',
+              border: InputBorder.none,
+              isDense: true,
+              contentPadding: EdgeInsets.zero,
+              hintText: '00',
+              hintStyle: textTheme.displayLarge?.copyWith(
+                color: colorScheme.secondary,
+              ),
+            ),
+            onChanged: (value) => ref.read(strokeRateProvider.notifier).state =
+                int.tryParse(value) ?? 0,
+          ),
+        ),
+        Text(
+          'cyc/min',
+          textAlign: TextAlign.center,
+          style: textTheme.headlineLarge?.copyWith(
+            color: colorScheme.onPrimary,
+          ),
+        ),
+      ],
+    );
+  }
+
+  String formatTime(int time) {
+    if (time <= 0) {
+      return '';
+    }
+    return time.toString();
+  }
+}
