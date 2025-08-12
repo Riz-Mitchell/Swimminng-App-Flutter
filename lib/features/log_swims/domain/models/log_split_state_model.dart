ae84e7065cb77647b6e23aa112101661f8caba7f
Riley-Mac <rileymitch117@gmail.com>
2025-08-12 12:11:55 +1000

    Refactor LogSplitStateModel to include distance, time, rate, and count properties, and add validation methods for these fields

--- lib/features/log_swims/domain/models/log_split_state_model.dart
+++ lib/features/log_swims/domain/models/log_split_state_model.dart
@@ -1,129 +1,67 @@
 import 'package:swimming_app_frontend/features/log_swims/domain/enum/status_log_split_enum.dart';
+import 'package:swimming_app_frontend/features/log_swims/domain/models/split_model.dart';
 import 'package:swimming_app_frontend/features/swims/enum/event_enum.dart';
 
 class LogSplitStateModel {
   final StatusLogSplitEnum status;
   final EventEnum event;
+  final int? distance;
+  final double? time;
+  final int? rate;
+  final int? count;
 
   LogSplitStateModel({
     this.status = StatusLogSplitEnum.selectDistance,
     required this.event,
+    this.distance,
+    this.time,
+    this.rate,
+    this.count,
   });
 
-  LogSplitStateModel copyWith({StatusLogSplitEnum? status, EventEnum? event}) {
+  LogSplitStateModel copyWith({
+    StatusLogSplitEnum? status,
+    EventEnum? event,
+    int? distance,
+    double? time,
+    int? rate,
+    int? count,
+  }) {
     return LogSplitStateModel(
       status: status ?? this.status,
       event: event ?? this.event,
+      distance: distance,
+      time: time,
+      rate: rate,
+      count: count,
     );
   }
 
-  List<int> getAvailableSplitDistances() {
-    switch (event) {
-      case EventEnum.none:
-        return [];
-      case EventEnum.freestyle50:
-        return [15, 20, 25, 30, 35, 40, 45, 50];
-      case EventEnum.butterfly100:
-      case EventEnum.backstroke100:
-      case EventEnum.breaststroke100:
-      case EventEnum.freestyle100:
-        return [
-          15,
-          20,
-          25,
-          30,
-          35,
-          40,
-          45,
-          50,
-          60,
-          65,
-          70,
-          75,
-          80,
-          85,
-          90,
-          95,
-          100,
-        ];
-      case EventEnum.individualMedley200:
-      case EventEnum.butterfly200:
-      case EventEnum.backstroke200:
-      case EventEnum.breaststroke200:
-      case EventEnum.freestyle200:
-        return [15, 25, 50, 75, 100, 125, 150, 175, 200];
-      case EventEnum.individualMedley400:
-      case EventEnum.freestyle400:
-        return [
-          25,
-          50,
-          75,
-          100,
-          125,
-          150,
-          175,
-          200,
-          225,
-          250,
-          275,
-          300,
-          325,
-          350,
-          375,
-          400,
-        ];
-      case EventEnum.freestyle800:
-        return [
-          50,
-          100,
-          150,
-          200,
-          250,
-          300,
-          350,
-          400,
-          450,
-          500,
-          550,
-          600,
-          650,
-          700,
-          750,
-          800,
-        ];
-      case EventEnum.freestyle1500:
-        return [
-          50,
-          100,
-          150,
-          200,
-          250,
-          300,
-          350,
-          400,
-          450,
-          500,
-          550,
-          600,
-          650,
-          700,
-          750,
-          800,
-          850,
-          900,
-          950,
-          1000,
-          1050,
-          1100,
-          1150,
-          1200,
-          1250,
-          1300,
-          1350,
-          1400,
-          1450,
-          1500,
-        ];
+  bool isDistanceValid() {
+    if (distance == null || distance! <= 0) {
+      return false;
     }
+    return true;
+  }
+
+  bool isTimeValid() {
+    if (time == null || time! <= 0) {
+      return false;
+    }
+    return true;
+  }
+
+  bool isRateValid() {
+    if (rate == null || rate! <= 0) {
+      return false;
+    }
+    return true;
+  }
+
+  bool isCountValid() {
+    if (count == null || count! <= 0) {
+      return false;
+    }
+    return true;
   }
 }
