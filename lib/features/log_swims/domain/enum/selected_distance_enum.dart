c86b70aa2bfe3bbefef44030f832d0e08ab08a9e
Riley-Mac <rileymitch117@gmail.com>
2025-08-12 10:56:51 +1000

    Add extension to SelectedDistanceEnum for integer distance mapping

--- lib/features/log_swims/domain/enum/selected_distance_enum.dart
+++ lib/features/log_swims/domain/enum/selected_distance_enum.dart
@@ -7,3 +7,24 @@ enum SelectedDistanceEnum {
   eightHundred,
   oneThousandFiveHundred,
 }
+
+extension SelectedDistanceEnumExtension on SelectedDistanceEnum {
+  static int getIntDistanceByEnum(SelectedDistanceEnum distance) {
+    switch (distance) {
+      case SelectedDistanceEnum.fifty:
+        return 50;
+      case SelectedDistanceEnum.oneHundred:
+        return 100;
+      case SelectedDistanceEnum.twoHundred:
+        return 200;
+      case SelectedDistanceEnum.fourHundred:
+        return 400;
+      case SelectedDistanceEnum.eightHundred:
+        return 800;
+      case SelectedDistanceEnum.oneThousandFiveHundred:
+        return 1500;
+      case SelectedDistanceEnum.unselected:
+        return 0; // or throw an error if unselected is not valid
+    }
+  }
+}
