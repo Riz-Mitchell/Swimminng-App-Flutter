92524b6f36636d9cfd4866f4a8af2221bed4e731
Riley-Mac <rileymitch117@gmail.com>
2025-08-12 10:03:27 +1000

    Add SelectedEventNotifier for managing event selection logic in swim logs

---
+++ lib/features/log_swims/application/selected_event_provider.dart
@@ -0,0 +1,49 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:swimming_app_frontend/features/log_swims/application/selected_distance_log_swims_provider.dart';
+import 'package:swimming_app_frontend/features/log_swims/application/selected_event_stroke_log_swims_provider.dart';
+import 'package:swimming_app_frontend/features/log_swims/domain/enum/selected_distance_enum.dart';
+import 'package:swimming_app_frontend/features/log_swims/domain/enum/stroke_enum.dart';
+import 'package:swimming_app_frontend/features/swims/enum/event_enum.dart';
+
+class SelectedEventNotifier extends Notifier<EventEnum> {
+  @override
+  EventEnum build() {
+    final eventStrokeMap = ref.watch(selectedEventStrokeLogSwimsProvider);
+    final eventDistance = ref.watch(selectedDistanceLogSwimsProvider);
+
+    final eventStrokeProvider = ref.read(
+      selectedEventStrokeLogSwimsProvider.notifier,
+    );
+
+    if (eventStrokeProvider.isValidSelection()) {
+      if (eventStrokeProvider.isIndividualMedleySelected()) {
+        return EventEnumExtension.getEventByStrokeAndDistance(
+          stroke: null,
+          distance: SelectedDistanceEnumExtension.getIntDistanceByEnum(
+            eventDistance,
+          ),
+        );
+      } else if (eventStrokeProvider.isFreestyleSelected()) {
+        return EventEnumExtension.getEventByStrokeAndDistance(
+          stroke: StrokeEnum.freestyle,
+          distance: SelectedDistanceEnumExtension.getIntDistanceByEnum(
+            eventDistance,
+          ),
+        );
+      } else {
+        // If a valid stroke is selected but not freestyle or IM, return the event based on the selected stroke
+        final selectedStroke = eventStrokeMap.keys.firstWhere(
+          (stroke) => eventStrokeMap[stroke] == true,
+        );
+        return EventEnumExtension.getEventByStrokeAndDistance(
+          stroke: selectedStroke,
+          distance: SelectedDistanceEnumExtension.getIntDistanceByEnum(
+            eventDistance,
+          ),
+        );
+      }
+    } else {
+      return EventEnum.none;
+    }
+  }
+}
