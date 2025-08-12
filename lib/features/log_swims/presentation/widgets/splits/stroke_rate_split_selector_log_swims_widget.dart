225085bf5567344fa67f361b9c9afd147b9450c3
Riley-Mac <rileymitch117@gmail.com>
2025-08-12 10:58:01 +1000

    Refactor StrokeRateSplitSelectorLogSwimsWidget to integrate navigation and enhance UI with SplitModalHeader and StrokeRateInput components

--- lib/features/log_swims/presentation/widgets/splits/stroke_rate_split_selector_log_swims_widget.dart
+++ lib/features/log_swims/presentation/widgets/splits/stroke_rate_split_selector_log_swims_widget.dart
@@ -1,24 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:swimming_app_frontend/features/log_swims/application/log_split_log_swims_provider.dart';
+import 'package:swimming_app_frontend/features/log_swims/domain/enum/status_log_split_enum.dart';
+import 'package:swimming_app_frontend/features/log_swims/presentation/widgets/splits/split_modal_header_log_swims_widget.dart';
+import 'package:swimming_app_frontend/features/log_swims/presentation/widgets/splits/split_modal_shell_widget.dart';
+import 'package:swimming_app_frontend/features/log_swims/presentation/widgets/splits/stroke_rate_input_log_swims_widget.dart';
 import 'package:swimming_app_frontend/shared/presentation/widgets/metric_button_widget.dart';
 
 class StrokeRateSplitSelectorLogSwimsWidget extends ConsumerWidget {
-  const StrokeRateSplitSelectorLogSwimsWidget({super.key});
+  final void Function(BuildContext context, StatusLogSplitEnum nextStep)
+  navigateToStep;
+
+  const StrokeRateSplitSelectorLogSwimsWidget({
+    super.key,
+    required this.navigateToStep,
+  });
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final textTheme = Theme.of(context).textTheme;
     final colorScheme = Theme.of(context).colorScheme;
 
-    return Column(
-      spacing: 40,
-      mainAxisAlignment: MainAxisAlignment.start,
+    final logSplitState = ref.watch(logSplitProvider);
+    final logSplitNotifier = ref.read(logSplitProvider.notifier);
+
+    return SplitModalShellWidget(
       children: [
-        Text(
-          'Split Rate',
-          style: textTheme.headlineLarge?.copyWith(
-            color: colorScheme.onPrimary,
+        SplitModalHeaderLogSwimsWidget(title: 'Split Stroke Rate'),
+        Container(
+          decoration: BoxDecoration(
+            color: colorScheme.onPrimaryContainer,
+            borderRadius: BorderRadius.circular(8),
+          ),
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // TimeInputLogSwimsWidget(
+              //   timeInputType: TimeInputTypeEnum.minutes,
+              //   subscriptText: 'm',
+              // ),
+              StrokeRateInputLogSwimsWidget(),
+            ],
           ),
         ),
         MetricButtonWidget(
@@ -26,7 +50,9 @@ class StrokeRateSplitSelectorLogSwimsWidget extends ConsumerWidget {
           isEnabled: true,
           onPressed: () {
             if (true) {
-              ref.read(logSplitProvider.notifier).navigateToNextStep(context);
+              ref
+                  .read(logSplitProvider.notifier)
+                  .navigateToNextStep(context, navigateToStep);
             }
           },
         ),
