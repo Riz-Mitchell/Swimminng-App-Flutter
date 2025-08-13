0075f187b42d9d2206dec55d7f2a50bace933c5f
Riley-Mac <rileymitch117@gmail.com>
2025-08-13 14:00:57 +1000

    Add selected_event_provider import to StrokeLogSwimsScreen for event handling

--- lib/features/log_swims/presentation/screens/stroke_log_swims_screen.dart
+++ lib/features/log_swims/presentation/screens/stroke_log_swims_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:swimming_app_frontend/features/log_swims/application/log_swim_provider.dart';
+import 'package:swimming_app_frontend/features/log_swims/application/selected_event_provider.dart';
 import 'package:swimming_app_frontend/features/log_swims/application/selected_event_stroke_log_swims_provider.dart';
 import 'package:swimming_app_frontend/features/log_swims/presentation/screens/log_swims_shell_screen.dart';
 import 'package:swimming_app_frontend/features/log_swims/presentation/widgets/header_log_swims_widget.dart';
