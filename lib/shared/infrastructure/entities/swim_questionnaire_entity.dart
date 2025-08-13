6a7a5f2e7d0a5cd23ab59b5087583d3c3bb16f4a
Riley-Mac <rileymitch117@gmail.com>
2025-08-13 15:40:09 +1000

    Add GetSwimQuestionnaireEntity and CreateSwimQuestionnaireEntity classes with detailed properties for swim questionnaire

--- lib/shared/infrastructure/entities/swim_questionnaire_entity.dart
+++ lib/shared/infrastructure/entities/swim_questionnaire_entity.dart
@@ -1,5 +1,55 @@
-class GetSwimQuestionnaireEntity {}
+import 'package:swimming_app_frontend/features/log_swims/domain/enum/questionnaire_options_enum.dart';
 
-class CreateSwimQuestionnaireEntity {}
+class GetSwimQuestionnaireEntity {
+  SelfTalkOptionsEnum selfTalk;
+  NervesOptionsEnum nerves;
+  EnergyLevelOptionsEnum energyLevel;
+  BreathingOptionsEnum breathing;
+  CatchFeelOptionsEnum catchFeel;
+  StrokeLengthOptionsEnum strokeLength;
+  KickTechniqueOptionsEnum kickTechnique;
+  KickThroughoutOptionsEnum kickThroughout;
+  HeadPositionOptionsEnum headPosition;
+  TurnOptionsEnum turn;
+
+  GetSwimQuestionnaireEntity({
+    required this.selfTalk,
+    required this.nerves,
+    required this.energyLevel,
+    required this.breathing,
+    required this.catchFeel,
+    required this.strokeLength,
+    required this.kickTechnique,
+    required this.kickThroughout,
+    required this.headPosition,
+    required this.turn,
+  });
+}
+
+class CreateSwimQuestionnaireEntity {
+  SelfTalkOptionsEnum selfTalk;
+  NervesOptionsEnum nerves;
+  EnergyLevelOptionsEnum energyLevel;
+  BreathingOptionsEnum breathing;
+  CatchFeelOptionsEnum catchFeel;
+  StrokeLengthOptionsEnum strokeLength;
+  KickTechniqueOptionsEnum kickTechnique;
+  KickThroughoutOptionsEnum kickThroughout;
+  HeadPositionOptionsEnum headPosition;
+  TurnOptionsEnum turn;
+
+  CreateSwimQuestionnaireEntity({
+    required this.selfTalk,
+    required this.nerves,
+    required this.energyLevel,
+    required this.breathing,
+    required this.catchFeel,
+    required this.strokeLength,
+    required this.kickTechnique,
+    required this.kickThroughout,
+    required this.headPosition,
+    required this.turn,
+  });
+}
 
 class UpdateSwimQuestionnaireEntity {}
