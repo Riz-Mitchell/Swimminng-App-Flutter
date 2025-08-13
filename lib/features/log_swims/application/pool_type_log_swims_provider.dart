6f925b7a7a5adc89a954df3eedf037c398cb76fb
Riley-Mac <rileymitch117@gmail.com>
2025-08-13 14:00:01 +1000

    Remove unused pool type provider from LogSwims application

--- lib/features/log_swims/application/pool_type_log_swims_provider.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:swimming_app_frontend/features/log_swims/domain/enum/selected_pool_type_enum.dart';
-
-final selectedPoolTypeProvider = StateProvider<SelectedPoolTypeEnum>(
-  (ref) => SelectedPoolTypeEnum.unselected,
-);
