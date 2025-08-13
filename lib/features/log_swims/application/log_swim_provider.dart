1094eb51ae7882ba9a3dd784e16b8e2942700ab8
Riley-Mac <rileymitch117@gmail.com>
2025-08-13 13:59:55 +1000

    Enhance LogSwimProvider to manage pool type, event, and questionnaire updates; add methods to reset splits and print current state

--- lib/features/log_swims/application/log_swim_provider.dart
+++ lib/features/log_swims/application/log_swim_provider.dart
@@ -1,23 +1,27 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:swimming_app_frontend/features/log_swims/application/log_split_log_swims_provider.dart';
-import 'package:swimming_app_frontend/features/log_swims/application/pool_type_log_swims_provider.dart';
 import 'package:swimming_app_frontend/features/log_swims/application/post_swim_questionnaire_log_swims_provider.dart';
 import 'package:swimming_app_frontend/features/log_swims/application/selected_distance_log_swims_provider.dart';
 import 'package:swimming_app_frontend/features/log_swims/application/selected_event_stroke_log_swims_provider.dart';
+import 'package:swimming_app_frontend/features/log_swims/domain/enum/questionnaire_options_enum.dart';
 import 'package:swimming_app_frontend/features/log_swims/domain/enum/selected_pool_type_enum.dart';
 import 'package:swimming_app_frontend/features/log_swims/domain/enum/status_log_swim_enum.dart';
 import 'package:swimming_app_frontend/features/log_swims/domain/models/log_split_state_model.dart';
 import 'package:swimming_app_frontend/features/log_swims/domain/models/log_swim_state_model.dart';
+import 'package:swimming_app_frontend/features/log_swims/domain/models/post_swim_questionnaire_model.dart';
 import 'package:swimming_app_frontend/features/log_swims/domain/models/split_model.dart';
 import 'package:swimming_app_frontend/features/swims/enum/event_enum.dart';
 import 'package:swimming_app_frontend/shared/application/providers/router_provider.dart';
 
 class LogSwimProvider extends Notifier<LogSwimStateModel> {
   @override
-  LogSwimStateModel build() => LogSwimStateModel(
-    event: EventEnum.none,
-    status: StatusLogSwimsEnum.selectPoolType,
-  );
+  LogSwimStateModel build() {
+    return LogSwimStateModel(
+      poolType: SelectedPoolTypeEnum.unselected,
+      event: EventEnum.none,
+      status: StatusLogSwimsEnum.selectPoolType,
+      questionnaire: PostSwimQuestionnaireModel(),
+    );
+  }
 
   void navigateToPrevStep() {
     final currentStatus = state.status;
@@ -92,11 +96,15 @@ class LogSwimProvider extends Notifier<LogSwimStateModel> {
 
   void _reset() {
     state = state.copyWith(status: StatusLogSwimsEnum.selectPoolType);
-    ref.invalidate(selectedPoolTypeProvider);
     ref.invalidate(selectedEventStrokeLogSwimsProvider);
     ref.invalidate(selectedDistanceLogSwimsProvider);
     ref.invalidate(postSwimQuestionnaireLogSwimsProvider);
     ref.invalidateSelf();
+  }
+
+  void resetSplitsOnEventChange() {
+    // Reset splits when event changes
+    state = state.copyWith(splits: []);
   }
 
   void addSplit(LogSplitStateModel splitState) {
@@ -109,12 +117,12 @@ class LogSwimProvider extends Notifier<LogSwimStateModel> {
     final SplitModel newSplit = SplitModel(
       intervalDistance: splitState.distance!,
       intervalTime: splitState.time!,
-      intervalStrokeRate: splitState.rate!,
-      intervalStrokeCount: splitState.count!,
+      intervalStrokeRate: splitState.rate,
+      intervalStrokeCount: splitState.count,
     );
     final List<SplitModel> updatedSplits = [...currentSplits, newSplit];
     updatedSplits.sort(
-      (a, b) => a.intervalDistance.compareTo(b.intervalDistance),
+      (a, b) => b.intervalDistance.compareTo(a.intervalDistance),
     );
 
     state = state.copyWith(splits: updatedSplits);
@@ -122,9 +130,51 @@ class LogSwimProvider extends Notifier<LogSwimStateModel> {
 
   void removeSplit(SplitModel split) {
     final currentSplits = state.splits;
+
     final updatedSplits = List<SplitModel>.from(currentSplits);
     updatedSplits.remove(split);
+
+    updatedSplits.sort(
+      (a, b) => b.intervalDistance.compareTo(a.intervalDistance),
+    );
+
     state = state.copyWith(splits: updatedSplits);
+  }
+
+  void updatePoolType(SelectedPoolTypeEnum poolType) {
+    state = state.copyWith(poolType: poolType);
+  }
+
+  void updateEvent(EventEnum event) {
+    state = state.copyWith(event: event);
+  }
+
+  // Automatically updates the questionnaire when questionnaire is changes
+  void updateQuestionnaire() {
+    print('questionnaire updated');
+    final questionnaire = ref.watch(postSwimQuestionnaireLogSwimsProvider);
+
+    state = state.copyWith(questionnaire: questionnaire);
+  }
+
+  void printState() {
+    print('Current Log Swim State:');
+    print('Pool Type: ${state.poolType}');
+    print('Event: ${state.event}');
+    print('Status: ${state.status}');
+    print('Splits: ${state.splits.length}');
+    for (var split in state.splits) {
+      print(
+        'Split - Distance: ${split.intervalDistance}, Time: ${split.intervalTime}, Rate: ${split.intervalStrokeRate}, Count: ${split.intervalStrokeCount}',
+      );
+    }
+    print('Questionnaire: ${state.questionnaire}');
+    print(
+      'motivationalPositive: ${state.questionnaire.selfTalk[SelfTalkOptionsEnum.motivationalPositive]}',
+    );
+    print(
+      'motivationalNegative: ${state.questionnaire.selfTalk[SelfTalkOptionsEnum.motivationalNegative]}',
+    );
   }
 }
 
