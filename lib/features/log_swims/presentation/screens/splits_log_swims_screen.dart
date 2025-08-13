d87ce17eb513af3990996f79f31aeed33d63e94c
Riley-Mac <rileymitch117@gmail.com>
2025-08-13 14:00:54 +1000

    Refactor SplitsLogSwimsScreen to integrate current splits state, enhancing UI responsiveness and user guidance

--- lib/features/log_swims/presentation/screens/splits_log_swims_screen.dart
+++ lib/features/log_swims/presentation/screens/splits_log_swims_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:swimming_app_frontend/features/log_swims/application/log_swim_provider.dart';
 import 'package:swimming_app_frontend/features/log_swims/presentation/screens/log_swims_shell_screen.dart';
 import 'package:swimming_app_frontend/features/log_swims/presentation/widgets/header_log_swims_widget.dart';
+import 'package:swimming_app_frontend/features/log_swims/presentation/widgets/splits/split_card_log_swims_widget.dart';
 import 'package:swimming_app_frontend/features/log_swims/presentation/widgets/splits/split_modal_log_swims_widget.dart';
 import 'package:swimming_app_frontend/shared/presentation/widgets/metric_button_widget.dart';
 
@@ -13,6 +14,8 @@ class SplitsLogSwimsScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final textTheme = Theme.of(context).textTheme;
     final colorScheme = Theme.of(context).colorScheme;
+
+    final currentSplits = ref.watch(logSwimProvider).splits;
 
     return LogSwimsShellScreen(
       child: Column(
@@ -26,8 +29,37 @@ class SplitsLogSwimsScreen extends ConsumerWidget {
             style: textTheme.displaySmall?.copyWith(color: colorScheme.primary),
           ),
           SplitModalLogSwimsWidget(),
+          // if (currentSplits.isEmpty)
+          //   Container(
+          //     decoration: BoxDecoration(
+          //       color: colorScheme.surface,
+          //       borderRadius: BorderRadius.circular(20),
+          //     ),
+          //     padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          //     margin: EdgeInsets.symmetric(horizontal: 40),
+          //     child: Text(
+          //       textAlign: TextAlign.center,
+          //       'Tap the \'+\' button to get started!',
+          //       style: textTheme.bodyMedium?.copyWith(
+          //         color: colorScheme.secondary,
+          //       ),
+          //     ),
+          //   )
+          // else
+          ...currentSplits.map(
+            (split) => SplitCardLogSwimsWidget(
+              distance: split.intervalDistance,
+              time: split.intervalTime,
+              count: split.intervalStrokeCount,
+              rate: split.intervalStrokeRate,
+              splitRef: split,
+            ),
+          ),
           MetricButtonWidget(
-            text: 'Next',
+            text: (currentSplits.isEmpty)
+                ? 'Tap the \'+\' button to get started!'
+                : 'Next',
+            isEnabled: currentSplits.isNotEmpty,
             onPressed: () {
               if (true) {
                 ref.read(logSwimProvider.notifier).navigateToNextStep();
