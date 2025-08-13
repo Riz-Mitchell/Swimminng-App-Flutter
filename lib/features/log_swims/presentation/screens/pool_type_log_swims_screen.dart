bccb2ca344a1dd0f67358d9e4f2b8a2a73601a69
Riley-Mac <rileymitch117@gmail.com>
2025-08-13 14:00:47 +1000

    Refactor PoolTypeLogSwimsScreen to use logSwimProvider for selected pool type, improving state management

--- lib/features/log_swims/presentation/screens/pool_type_log_swims_screen.dart
+++ lib/features/log_swims/presentation/screens/pool_type_log_swims_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:swimming_app_frontend/features/log_swims/application/log_swim_provider.dart';
-import 'package:swimming_app_frontend/features/log_swims/application/pool_type_log_swims_provider.dart';
 import 'package:swimming_app_frontend/features/log_swims/domain/enum/selected_pool_type_enum.dart';
 import 'package:swimming_app_frontend/features/log_swims/presentation/screens/log_swims_shell_screen.dart';
 import 'package:swimming_app_frontend/features/log_swims/presentation/widgets/pool_type_selector_log_swims_widget.dart';
@@ -16,7 +15,7 @@ class PoolTypeLogSwimsScreen extends ConsumerWidget {
     final textTheme = Theme.of(context).textTheme;
     final colorScheme = Theme.of(context).colorScheme;
 
-    final selectedPoolType = ref.watch(selectedPoolTypeProvider);
+    final selectedPoolType = ref.watch(logSwimProvider).poolType;
 
     final isNextEnabled = selectedPoolType != SelectedPoolTypeEnum.unselected;
 
