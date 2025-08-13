8fdbb85a37f4b2aab45265a2caff1abdf916dba3
Riley-Mac <rileymitch117@gmail.com>
2025-08-13 14:01:12 +1000

    Refactor PoolTypeSelectorLogSwimsWidget to use logSwimProvider for pool type management

--- lib/features/log_swims/presentation/widgets/pool_type_selector_log_swims_widget.dart
+++ lib/features/log_swims/presentation/widgets/pool_type_selector_log_swims_widget.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:swimming_app_frontend/features/log_swims/application/pool_type_log_swims_provider.dart';
+import 'package:swimming_app_frontend/features/log_swims/application/log_swim_provider.dart';
 import 'package:swimming_app_frontend/features/log_swims/domain/enum/selected_pool_type_enum.dart';
 import 'package:swimming_app_frontend/features/log_swims/presentation/widgets/pool_type_button_log_swims_widget.dart';
 
@@ -9,10 +9,8 @@ class PoolTypeSelectorLogSwimsWidget extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final selectedPoolTypeState = ref.watch(selectedPoolTypeProvider);
-    final selectedPoolTypeNotifier = ref.read(
-      selectedPoolTypeProvider.notifier,
-    );
+    final selectedPoolTypeState = ref.watch(logSwimProvider).poolType;
+    final logSwimNotifier = ref.read(logSwimProvider.notifier);
 
     return Wrap(
       spacing: 10,
