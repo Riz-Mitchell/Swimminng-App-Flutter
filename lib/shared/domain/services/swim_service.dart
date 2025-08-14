28cec71b2a815d2b927c3d7a1704ba2ff484577d
Riley-Mac <rileymitch117@gmail.com>
2025-08-14 16:36:43 +1000

    Refactor SwimService to use SwimEntity and update query parameters for improved data handling

--- lib/shared/domain/services/swim_service.dart
+++ lib/shared/domain/services/swim_service.dart
@@ -1,43 +1,45 @@
 import 'package:swimming_app_frontend/features/swims/enum/time_period_enum.dart';
-import 'package:swimming_app_frontend/features/swims/domain/models/swim_model.dart';
-import 'package:swimming_app_frontend/features/swims/infrastructure/swim_repository.dart';
+import 'package:swimming_app_frontend/shared/infrastructure/entities/swim_entity.dart';
+import 'package:swimming_app_frontend/shared/infrastructure/repository/swim_repository.dart';
 
 class SwimService {
   final SwimRepository _swimRepository;
 
   SwimService(this._swimRepository);
 
-  Future<List<GetSwimResDTO>> getSwimsByQuery(GetSwimsQuery querySchema) async {
-    List<GetSwimResDTO> swims = await _swimRepository.getAllSwimsReq(
+  Future<List<GetSwimEntity>> getSwimsByQuery(
+    QuerySwimEntity querySchema,
+  ) async {
+    List<GetSwimEntity> swims = await _swimRepository.getAllSwimsReq(
       querySchema,
     );
 
     return swims;
   }
 
-  Future<GetSwimResDTO> getSwimById(String swimId) async {
+  Future<GetSwimEntity> getSwimById(String swimId) async {
     return await _swimRepository.getSwimReq(swimId);
   }
 
-  Future<GetSwimResDTO> getLatestSwim() async {
-    GetSwimsQuery querySchema = GetSwimsQuery(page: 1, pageSize: 1);
+  Future<GetSwimEntity> getLatestSwim() async {
+    QuerySwimEntity querySchema = QuerySwimEntity(page: 1, pageSize: 1);
 
-    List<GetSwimResDTO> singleItemList = await _swimRepository.getAllSwimsReq(
+    List<GetSwimEntity> singleItemList = await _swimRepository.getAllSwimsReq(
       querySchema,
     );
 
     return singleItemList.first;
   }
 
-  Future<List<GetSwimResDTO>> getSwimsByTimePeriodAsync(
-    TimePeriod timePeriod,
+  Future<List<GetSwimEntity>> getSwimsByTimePeriodEnumAsync(
+    TimePeriodEnum timePeriod,
   ) async {
     const int maxPageSize = 20;
     int currentPage = 1;
-    List<GetSwimResDTO> allSwims = [];
+    List<GetSwimEntity> allSwims = [];
 
     while (true) {
-      final query = GetSwimsQuery(
+      final query = QuerySwimEntity(
         timePeriod: timePeriod,
         page: currentPage,
         pageSize: maxPageSize,
@@ -56,7 +58,7 @@ class SwimService {
     return allSwims;
   }
 
-  Future<void> createSwim(CreateSwimReqDTO schema) async {
+  Future<void> createSwim(CreateSwimEntity schema) async {
     await _swimRepository.createSwimReq(schema);
   }
 }
