a78d816ae81170c96f71028466021f6632821cf4
Riley-Mac <rileymitch117@gmail.com>
2025-08-15 17:07:25 +1000

    Rename AusRequestEntity to AusResponseEntity for clarity and update factory method accordingly

--- lib/external/aus/infrastructure/entities/aus_request_entity.dart
+++ lib/external/aus/infrastructure/entities/aus_request_entity.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
 
 @immutable
-class AusRequestEntity<T> {
+class AusResponseEntity<T> {
   final T data;
   final bool success;
   final int code;
 
-  const AusRequestEntity({
+  const AusResponseEntity({
     required this.data,
     required this.success,
     required this.code,
   });
 
-  factory AusRequestEntity.fromJson(
+  factory AusResponseEntity.fromJson(
     Map<String, dynamic> json,
     T Function(Map<String, dynamic>) fromJsonT,
   ) {
-    return AusRequestEntity<T>(
+    return AusResponseEntity<T>(
       data: fromJsonT(json['data'] as Map<String, dynamic>),
       success: json['success'] as bool,
       code: json['code'] as int,
