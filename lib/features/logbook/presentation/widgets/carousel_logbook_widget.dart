d7712abc2ec3c60be3ad22f351620b254527cb30
Riley-Mac <rileymitch117@gmail.com>
2025-08-15 13:39:38 +1000

    Enhance CarouselLogbookWidget to utilize state management for swim logbook and selected day

--- lib/features/logbook/presentation/widgets/carousel_logbook_widget.dart
+++ lib/features/logbook/presentation/widgets/carousel_logbook_widget.dart
@@ -2,7 +2,12 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:swimming_app_frontend/features/logbook/application/logbook_provider.dart';
+import 'package:swimming_app_frontend/features/logbook/application/selected_day_logbook_provider.dart';
+import 'package:swimming_app_frontend/features/logbook/application/selected_swim_logbook_provider.dart';
+import 'package:swimming_app_frontend/features/logbook/domain/models/logbook_state_model.dart';
 import 'package:swimming_app_frontend/features/logbook/presentation/widgets/swim_card_logbook_widget.dart';
+import 'package:swimming_app_frontend/shared/infrastructure/entities/swim_entity.dart';
 
 class CarouselLogbookWidget extends ConsumerWidget {
   const CarouselLogbookWidget({Key? key}) : super(key: key);
@@ -12,6 +17,14 @@ class CarouselLogbookWidget extends ConsumerWidget {
     final colorScheme = Theme.of(context).colorScheme;
     final textTheme = Theme.of(context).textTheme;
     final screenWidth = MediaQuery.of(context).size.width;
+
+    final carouselController = ref.watch(carouselControllerProvider);
+    final selectedIndex = ref.watch(selectedSwimLogbookProvider);
+    print('selected index: $selectedIndex');
+
+    final selectedDate = ref.watch(selectedDayLogbookProvider);
+
+    final logbookState = ref.watch(logbookProvider);
 
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -42,32 +55,44 @@ class CarouselLogbookWidget extends ConsumerWidget {
           ),
         ),
         CarouselSlider(
+          carouselController: carouselController,
           options: CarouselOptions(
+            initialPage: selectedIndex,
             viewportFraction: 1,
             enlargeCenterPage: true,
             autoPlay: false,
             enableInfiniteScroll: false,
+            onPageChanged: (index, reason) =>
+                ref.read(selectedSwimLogbookProvider.notifier).setIndex(index),
           ),
-          items: [
-            SwimCardLogbookWidget(
-              asset: 'assets/svg/Butterfly.svg',
-              swim: null,
-            ),
-            SwimCardLogbookWidget(
-              asset: 'assets/svg/Freestyle.svg',
-              swim: null,
-            ),
-            SwimCardLogbookWidget(
-              asset: 'assets/svg/Backstroke.svg',
-              swim: null,
-            ),
-            SwimCardLogbookWidget(
-              asset: 'assets/svg/Breaststroke.svg',
-              swim: null,
-            ),
-          ],
+          items: [..._buildSwimCards(logbookState, selectedDate)],
         ),
       ],
     );
+  }
+
+  List<Widget> _buildSwimCards(
+    AsyncValue<LogbookStateModel> logbookState,
+    DateTime time,
+  ) {
+    return logbookState.when(
+      data: (data) {
+        final dayData = data.getDayData(time.year, time.month, time.day);
+
+        if (dayData == null) {
+          return [];
+        }
+
+        return dayData.swims.map((swim) {
+          return _buildSwimCardWidget(swim);
+        }).toList();
+      },
+      loading: () => [],
+      error: (error, stack) => [],
+    );
+  }
+
+  Widget _buildSwimCardWidget(GetSwimEntity swim) {
+    return SwimCardLogbookWidget(swim: swim);
   }
 }
