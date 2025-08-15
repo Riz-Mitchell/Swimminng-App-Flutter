3404ea5a3f3617d8bbe2349f10427dd8b5275e93
Riley-Mac <rileymitch117@gmail.com>
2025-08-15 18:22:59 +1000

    Add AusRepository class for fetching swim and split data from API

---
+++ lib/external/aus/infrastructure/repositories/aus_repository.dart
@@ -0,0 +1,37 @@
+import 'package:swimming_app_frontend/external/aus/infrastructure/entities/aus_split_entity.dart';
+import 'package:swimming_app_frontend/external/aus/infrastructure/entities/aus_swim_entity.dart';
+import 'package:swimming_app_frontend/shared/infrastructure/api.dart';
+
+class AusRepository {
+  final ApiClient _apiClient;
+
+  AusRepository(this._apiClient);
+
+  Future<List<GetAusSwimEntity>> getAllAusSwims(
+    QueryReqAusSwimEntity query,
+  ) async {
+    final res = await _apiClient.get('/results/results', query: query.toJson());
+
+    if (res == null) {
+      throw Exception('Failed to get all Aus swims: No response from server.');
+    }
+
+    return (res.data as List)
+        .map((swim) => GetAusSwimEntity.fromJson(swim))
+        .toList();
+  }
+
+  Future<List<GetAusSplitEntity>> getAllSplitsForSwim(
+    QueryReqAusSplitEntity query,
+  ) async {
+    final res = await _apiClient.get('/results/splits', query: query.toJson());
+
+    if (res == null) {
+      throw Exception('Failed to get all Aus splits: No response from server.');
+    }
+
+    return (res.data as List)
+        .map((split) => GetAusSplitEntity.fromJson(split))
+        .toList();
+  }
+}
