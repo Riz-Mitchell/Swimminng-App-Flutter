4fd48317a820223d71b0558cb8e76ee41beb2dd6
Riley-Mac <rileymitch117@gmail.com>
2025-08-18 09:29:59 +1000

    Add GetAusParticipantEntity class for participant data representation and JSON parsing

--- lib/external/aus/infrastructure/entities/aus_participant_entity.dart
+++ lib/external/aus/infrastructure/entities/aus_participant_entity.dart
@@ -1,4 +1,22 @@
+import 'package:flutter/material.dart';
+
 @immutable
-class AusParticipantEntity {
-  final String 
+class GetAusParticipantEntity {
+  final String participantId;
+  final String fullName;
+  final String club;
+
+  const GetAusParticipantEntity({
+    required this.participantId,
+    required this.fullName,
+    required this.club,
+  });
+
+  factory GetAusParticipantEntity.fromJson(Map<String, dynamic> json) {
+    return GetAusParticipantEntity(
+      participantId: json['participantId'] as String,
+      fullName: json['fullName'] as String,
+      club: json['club'] as String,
+    );
+  }
 }
