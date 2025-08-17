3009806845f8f7a3efd0ba127dfeb68f4327ae71
Riley-Mac <rileymitch117@gmail.com>
2025-08-18 09:29:53 +1000

    Refactor AusRepository to improve swim and split data retrieval, and add swimmer search functionality

--- lib/external/aus/infrastructure/repositories/aus_repository.dart
+++ lib/external/aus/infrastructure/repositories/aus_repository.dart
@@ -1,3 +1,7 @@
+import 'dart:core';
+
+import 'package:swimming_app_frontend/external/aus/infrastructure/entities/aus_participant_entity.dart';
+import 'package:swimming_app_frontend/external/aus/infrastructure/entities/aus_request_entity.dart';
 import 'package:swimming_app_frontend/external/aus/infrastructure/entities/aus_split_entity.dart';
 import 'package:swimming_app_frontend/external/aus/infrastructure/entities/aus_swim_entity.dart';
 import 'package:swimming_app_frontend/shared/infrastructure/api.dart';
@@ -7,7 +11,7 @@ class AusRepository {
 
   AusRepository(this._apiClient);
 
-  Future<List<GetAusSwimEntity>> getAllAusSwimsAsync(
+  Future<QueryResAusSwimEntity> getSwimsQueryAsync(
     QueryReqAusSwimEntity query,
   ) async {
     final res = await _apiClient.get('/results/results', query: query.toJson());
@@ -16,12 +20,13 @@ class AusRepository {
       throw Exception('Failed to get all Aus swims: No response from server.');
     }
 
-    return (res.data as List)
-        .map((swim) => GetAusSwimEntity.fromJson(swim))
-        .toList();
+    final AusResponseEntity<QueryResAusSwimEntity> ausResponse =
+        res.data as AusResponseEntity<QueryResAusSwimEntity>;
+
+    return ausResponse.data;
   }
 
-  Future<List<GetAusSplitEntity>> getAllSplitsForSwimAsync(
+  Future<List<GetAusSplitEntity>> getSplitsForRaceAsync(
     String raceResultId,
     QueryReqAusSplitEntity query,
   ) async {
@@ -34,8 +39,27 @@ class AusRepository {
       throw Exception('Failed to get all Aus splits: No response from server.');
     }
 
-    return (res.data as List)
-        .map((split) => GetAusSplitEntity.fromJson(split))
-        .toList();
+    final AusResponseEntity<List<GetAusSplitEntity>> ausResponse =
+        res.data as AusResponseEntity<List<GetAusSplitEntity>>;
+
+    return ausResponse.data;
+  }
+
+  Future<List<GetAusParticipantEntity>> getSwimmersAsync(
+    String searchTerm,
+  ) async {
+    final res = await _apiClient.get(
+      '/results/swimmers',
+      query: {'searchTerm': searchTerm},
+    );
+
+    if (res == null) {
+      throw Exception('Failed to get swimmers: No response from server.');
+    }
+
+    final AusResponseEntity<List<GetAusParticipantEntity>> ausResponse =
+        res.data as AusResponseEntity<List<GetAusParticipantEntity>>;
+
+    return ausResponse.data;
   }
 }
