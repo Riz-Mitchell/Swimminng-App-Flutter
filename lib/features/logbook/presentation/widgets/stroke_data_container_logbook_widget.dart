6aac3eb68c1acbd37db6f0ba8dd48d89cc3808b2
Riley-Mac <rileymitch117@gmail.com>
2025-08-22 11:02:17 +1000

    Refactor StrokeDataContainerLogbookWidget to dynamically display stroke data based on selected date and logbook state

--- lib/features/logbook/presentation/widgets/stroke_data_container_logbook_widget.dart
+++ lib/features/logbook/presentation/widgets/stroke_data_container_logbook_widget.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:swimming_app_frontend/features/logbook/application/logbook_provider.dart';
+import 'package:swimming_app_frontend/features/logbook/application/selected_day_logbook_provider.dart';
+import 'package:swimming_app_frontend/features/logbook/domain/models/logbook_state_model.dart';
 import 'package:swimming_app_frontend/features/logbook/presentation/widgets/stroke_data_logbook_widget.dart';
 import 'package:swimming_app_frontend/shared/enum/event_enum.dart';
 
@@ -11,6 +14,9 @@ class StrokeDataContainerLogbookWidget extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final colorScheme = Theme.of(context).colorScheme;
     final textTheme = Theme.of(context).textTheme;
+
+    final logbookState = ref.watch(logbookProvider);
+    final selectedDate = ref.watch(selectedDayLogbookProvider);
 
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -23,7 +29,7 @@ class StrokeDataContainerLogbookWidget extends ConsumerWidget {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Text(
-                'Strokes Data',
+                'By Events',
                 style: textTheme.headlineSmall?.copyWith(
                   color: colorScheme.secondary,
                 ),
@@ -40,14 +46,44 @@ class StrokeDataContainerLogbookWidget extends ConsumerWidget {
             ],
           ),
         ),
-        StrokeDataLogbookWidget(
-          event: EventEnum.freestyle50,
-          highestPerOffPb: 4.25,
-          averagePerOffPb: 1,
-          lowestPerOffPb: -6.51,
-          isSelected: true,
-        ),
+        ..._buildStrokeDataWidgets(logbookState, selectedDate),
       ],
+    );
+  }
+
+  List<Widget> _buildStrokeDataWidgets(
+    AsyncValue<LogbookStateModel> logbookState,
+    DateTime time,
+  ) {
+    return logbookState.when(
+      data: (data) {
+        final dayData = data.getDayData(time.year, time.month, time.day);
+
+        if (dayData == null) {
+          return [];
+        }
+
+        final List<EventEnum> events = dayData.eventsSwumToday;
+
+        return events.map((event) {
+          final averagePerOffPb = dayData.calculateAveragePercentOffPbByEvent(
+            event,
+          );
+          final highestPerOffPb = dayData.calculateHighPercentOffPbByEvent(
+            event,
+          );
+          final lowestPerOffPb = dayData.calculateLowPercentOffPbByEvent(event);
+
+          return StrokeDataLogbookWidget(
+            event: event,
+            averagePerOffPb: averagePerOffPb,
+            highestPerOffPb: highestPerOffPb,
+            lowestPerOffPb: lowestPerOffPb,
+          );
+        }).toList();
+      },
+      loading: () => [],
+      error: (error, stack) => [Text('Error loading stroke data: $error')],
     );
   }
 }
