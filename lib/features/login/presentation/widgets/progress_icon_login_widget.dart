9d966caf3564ed08bffc938feb695aa0d059efcb
Riley-Mac <rileymitch117@gmail.com>
2025-08-26 21:24:54 +1000

    Add ProgressIconLoginWidget to display login process status

---
+++ lib/features/login/presentation/widgets/progress_icon_login_widget.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:swimming_app_frontend/features/login/application/login_provider.dart';
+import 'package:swimming_app_frontend/features/login/domain/enum/login_status_enum.dart';
+import 'package:swimming_app_frontend/shared/presentation/theme/metric_colors.dart';
+
+class ProgressIconLoginWidget extends ConsumerWidget {
+  final bool completed;
+  final bool isUsing;
+  final LoginStatusEnum status;
+
+  const ProgressIconLoginWidget({
+    super.key,
+    required this.completed,
+    this.isUsing = false,
+    required this.status,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final colorScheme = Theme.of(context).colorScheme;
+    final textTheme = Theme.of(context).textTheme;
+
+    if (completed) {
+      return Icon(Icons.circle, size: 12, color: metricBlue);
+    } else if (isUsing) {
+      return Container(
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          border: Border.all(
+            color: metricBlue, // Circle border color
+            width: 2,
+          ),
+        ),
+        child: _getStatusIcon(colorScheme),
+      );
+    } else {
+      return Icon(Icons.circle, size: 12, color: colorScheme.secondary);
+    }
+  }
+
+  Widget _getStatusIcon(ColorScheme colorScheme) {
+    String assetPath;
+    switch (status) {
+      case LoginStatusEnum.addPhoneNumber:
+        assetPath = 'assets/svg/smart_phone.svg';
+        break;
+      case LoginStatusEnum.verifyPhoneNumber:
+        assetPath = 'assets/svg/shield.svg';
+        break;
+      case LoginStatusEnum.done:
+        assetPath = 'assets/svg/tick.svg';
+        break;
+    }
+    return SvgPicture.asset(
+      assetPath,
+      width: 40,
+      height: 40,
+      colorFilter: ColorFilter.mode(colorScheme.primary, BlendMode.srcIn),
+    );
+  }
+}
