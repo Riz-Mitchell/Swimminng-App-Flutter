1347919a91373b39d781d8696bc38fe689c2f70e
Riley-Mac <rileymitch117@gmail.com>
2025-08-27 10:36:12 +1000

    Add Date of Birth picker with state management using Riverpod

--- lib/features/signup/presentation/widgets/dob_picker_signup_widget.dart
+++ lib/features/signup/presentation/widgets/dob_picker_signup_widget.dart
@@ -1,75 +1,40 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:swimming_app_frontend/features/signup/application/selected_date_signup_provider.dart';
 import 'package:swimming_app_frontend/features/signup/application/signup_provider.dart';
 
-class DobPickerSignupWidget extends ConsumerStatefulWidget {
+class DobPickerSignupWidget extends ConsumerWidget {
   const DobPickerSignupWidget({super.key});
 
   @override
-  ConsumerState<DobPickerSignupWidget> createState() => _DOBPickerState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final textTheme = Theme.of(context).textTheme;
+    final colorScheme = Theme.of(context).colorScheme;
+    final selectedDate = ref.watch(selectedDateSignupProvider);
 
-class _DOBPickerState extends ConsumerState<DobPickerSignupWidget> {
-  DateTime selectedDate = DateTime.now();
-
-  void _showDatePicker(BuildContext context) {
-    final signupNotifier = ref.read(signupProvider.notifier);
-
-    showCupertinoModalPopup(
-      context: context,
-      builder: (_) => Container(
-        height: 250,
-        color: CupertinoColors.systemBackground.resolveFrom(context),
+    return SizedBox(
+      height: 250,
+      child: CupertinoTheme(
+        data: CupertinoThemeData(
+          textTheme: CupertinoTextThemeData(
+            dateTimePickerTextStyle: textTheme.bodyLarge?.copyWith(
+              color: colorScheme.onBackground,
+            ),
+          ),
+        ),
         child: CupertinoDatePicker(
           mode: CupertinoDatePickerMode.date,
           initialDateTime: selectedDate,
           maximumDate: DateTime.now(),
           minimumYear: 1900,
           maximumYear: DateTime.now().year,
-          onDateTimeChanged: (DateTime newDate) {
-            setState(() {
-              selectedDate = newDate;
-            });
-            signupNotifier.updateSignupForm(dateOfBirth: newDate.toUtc());
+          onDateTimeChanged: (DateTime newDate) async {
+            ref.read(selectedDateSignupProvider.notifier).state = newDate;
+            await ref
+                .read(signupProvider.notifier)
+                .updateSignupForm(dateOfBirth: newDate.toUtc());
           },
-        ),
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final textTheme = Theme.of(context).textTheme;
-
-    return GestureDetector(
-      onTap: () => _showDatePicker(context),
-      child: Container(
-        padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 12),
-        decoration: BoxDecoration(
-          border: Border(
-            bottom: BorderSide(
-              color: Theme.of(context).colorScheme.primary,
-              width: 2,
-            ),
-          ),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              'Date of Birth',
-              style: textTheme.labelLarge?.copyWith(
-                color: Theme.of(context).colorScheme.secondary,
-              ),
-            ),
-            Text(
-              '${selectedDate.day}/${selectedDate.month}/${selectedDate.year}',
-              style: textTheme.bodyMedium?.copyWith(
-                color: Theme.of(context).colorScheme.onBackground,
-              ),
-            ),
-          ],
         ),
       ),
     );
