d3574ef5ccd368f71393a1040cb7bc4a6906a463
Riley-Mac <rileymitch117@gmail.com>
2025-09-01 12:46:58 +1000

    Refactor LaunchAppStartScreen to use ConsumerWidget and remove unnecessary state management

--- lib/features/app_start/presentation/screens/launch_app_start_screen.dart
+++ lib/features/app_start/presentation/screens/launch_app_start_screen.dart
@@ -1,36 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:swimming_app_frontend/features/app_start/application/providers/splash_status_provider.dart';
-import 'package:swimming_app_frontend/features/app_start/presentation/widgets/expanding_circle_app_start_widget.dart';
 import '../widgets/welcome_app_start_widget.dart';
 
-class LaunchAppStartScreen extends ConsumerStatefulWidget {
+class LaunchAppStartScreen extends ConsumerWidget {
   const LaunchAppStartScreen({super.key});
 
   @override
-  ConsumerState<LaunchAppStartScreen> createState() =>
-      _LaunchAppStartScreenState();
-}
-
-class _LaunchAppStartScreenState extends ConsumerState<LaunchAppStartScreen> {
-  @override
-  void initState() {
-    super.initState();
-    Future.microtask(() {
-      ref.read(splashStatusProvider.notifier).start();
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Scaffold(
-          backgroundColor: Theme.of(context).colorScheme.background,
-          body: Center(child: WelcomeAppStartWidget()), // visual only
-        ),
-        const ExpandingCircleAppStartWidget(), // reacts to controller state
-      ],
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(child: WelcomeAppStartWidget()), // visual only
     );
   }
 }
