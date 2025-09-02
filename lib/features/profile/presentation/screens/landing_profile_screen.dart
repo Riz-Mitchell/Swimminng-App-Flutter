2f20b29035726d826e1c4192a9a00da0b5236454
Riley-Mac <rileymitch117@gmail.com>
2025-09-02 15:09:39 +1000

    Refactor LandingProfileScreen to utilize profileProvider and enhance layout structure

--- lib/features/profile/presentation/screens/landing_profile_screen.dart
+++ lib/features/profile/presentation/screens/landing_profile_screen.dart
@@ -1,14 +1,13 @@
-import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:swimming_app_frontend/features/profile/application/profile_provider.dart';
 import 'package:swimming_app_frontend/features/profile/presentation/widgets/data_container_profile_widget.dart';
 import 'package:swimming_app_frontend/features/profile/presentation/widgets/carousel_profile_widget.dart';
-import 'package:swimming_app_frontend/features/profile/presentation/widgets/header_profile_widget.dart';
-import 'package:swimming_app_frontend/features/profile/presentation/widgets/stroke_distribution_radar_profile_widget.dart';
+import 'package:swimming_app_frontend/features/profile/presentation/widgets/hero_card_profile.dart';
 import 'package:swimming_app_frontend/shared/application/providers/auth_provider.dart';
 import 'package:swimming_app_frontend/shared/presentation/theme/metric_colors.dart';
 import 'package:swimming_app_frontend/shared/presentation/widgets/metric_button_widget.dart';
+import 'package:country_icons/country_icons.dart';
 
 class LandingProfileScreen extends ConsumerWidget {
   const LandingProfileScreen({super.key});
@@ -18,31 +17,49 @@ class LandingProfileScreen extends ConsumerWidget {
     final colorScheme = Theme.of(context).colorScheme;
     final textTheme = Theme.of(context).textTheme;
     final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
 
-    return Column(
-      spacing: 50,
-      children: [
-        HeaderProfileWidget(
-          name: 'Rizzle',
-          swims: 1450,
-          joinedDate: DateTime(2025, 1, 1),
-          friends: 231,
-        ),
-        // StrokeDistributionRadarProfileWidget(),
-        CarouselProfileWidget(),
+    final profileInfo = ref.watch(profileProvider);
 
-        DataContainerProfileWidget(),
-        SizedBox(height: 10),
+    return profileInfo.when(
+      error: (error, stackTrace) => SizedBox.shrink(),
+      loading: () => SizedBox.shrink(),
+      data: (value) {
+        return Column(
+          spacing: 50,
+          children: [
+            HeroCardProfile(user: value.user),
 
-        MetricButtonWidget(
-          text: 'Logout',
-          metricColor: metricRed,
-          onPressed: () async {
-            await ref.read(authControllerProvider.notifier).logout();
-          },
-        ),
-        SizedBox(height: 50),
-      ],
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 0,
+                left: 12,
+                right: 12,
+                bottom: 0,
+              ),
+              child: Column(
+                spacing: 50,
+                children: [
+                  // StrokeDistributionRadarProfileWidget(),
+                  CarouselProfileWidget(),
+
+                  DataContainerProfileWidget(),
+                  SizedBox(height: 10),
+
+                  MetricButtonWidget(
+                    text: 'Logout',
+                    metricColor: metricRed,
+                    onPressed: () async {
+                      await ref.read(authControllerProvider.notifier).logout();
+                    },
+                  ),
+                  SizedBox(height: 50),
+                ],
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
