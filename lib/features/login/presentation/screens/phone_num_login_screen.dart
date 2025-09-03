3424ce5965350c550d3f34ef5b69521a0d5f44af
Riley-Mac <rileymitch117@gmail.com>
2025-09-03 12:19:18 +1000

    Replace progress indicator with header widget in phone number login screen and add error message display for improved user feedback

--- lib/features/login/presentation/screens/phone_num_login_screen.dart
+++ lib/features/login/presentation/screens/phone_num_login_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:swimming_app_frontend/features/login/application/login_provider.dart';
 import 'package:swimming_app_frontend/features/login/presentation/screens/login_shell_screen.dart';
-import 'package:swimming_app_frontend/features/login/presentation/widgets/progress_indicator_login_widget.dart';
+import 'package:swimming_app_frontend/features/login/presentation/widgets/header_login_widget.dart';
 import 'package:swimming_app_frontend/shared/presentation/widgets/metric_button_widget.dart';
 import 'package:swimming_app_frontend/shared/presentation/widgets/phone_number_input_widget.dart';
 
@@ -13,6 +13,9 @@ class PhoneNumLoginScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final loginState = ref.watch(loginProvider);
     final loginNotifier = ref.read(loginProvider.notifier);
+
+    final textTheme = Theme.of(context).textTheme;
+    final colorScheme = Theme.of(context).colorScheme;
 
     return LoginShellScreen(
       child: Column(
@@ -30,17 +33,25 @@ class PhoneNumLoginScreen extends ConsumerWidget {
               ),
             ),
           ),
+          SizedBox(height: 20),
           PhoneNumInputWidget(
             onChanged: (value) {
               loginNotifier.updateLoginForm(phoneNumber: value);
             },
           ),
+          SizedBox(height: 20),
+          if (loginState.errorMessage != null)
+            Text(
+              textAlign: TextAlign.center,
+              '${loginState.errorMessage}',
+              style: textTheme.bodySmall?.copyWith(color: colorScheme.error),
+            ),
           SizedBox(height: 100),
           MetricButtonWidget(
             text: 'Next',
-            isEnabled: true,
+            isEnabled: (loginState.loginForm.isPhoneNumberValid()),
             onPressed: () async =>
-                await ref.read(loginProvider.notifier).navigateToNextStep(),
+                await ref.read(loginProvider.notifier).pushToLoginVerify(),
           ),
         ],
       ),
