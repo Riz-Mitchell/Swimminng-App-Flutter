fec8c9e5f6a21e680348c8a680d8a1524f6e0179
Riley-Mac <rileymitch117@gmail.com>
2025-09-12 14:42:25 +1000

    Add image handling functionality to UserService for profile picture updates

--- lib/shared/domain/services/user_service.dart
+++ lib/shared/domain/services/user_service.dart
@@ -1,15 +1,25 @@
+import 'dart:io';
+
+import 'package:mime/mime.dart';
 import 'package:swimming_app_frontend/features/signup/domain/models/login_form_model.dart';
 import 'package:swimming_app_frontend/features/signup/domain/models/signup_form_model.dart';
 import 'package:swimming_app_frontend/shared/infrastructure/entities/auth_entity.dart';
+import 'package:swimming_app_frontend/shared/infrastructure/entities/image_entity.dart';
 import 'package:swimming_app_frontend/shared/infrastructure/entities/user_entity.dart';
 import 'package:swimming_app_frontend/shared/infrastructure/repository/auth_repository.dart';
+import 'package:swimming_app_frontend/shared/infrastructure/repository/image_repository.dart';
 import 'package:swimming_app_frontend/shared/infrastructure/repository/user_repository.dart';
 
 class UserService {
   final UserRepository _userRepository;
   final AuthRepository _authRepository;
+  final ImageRepository _imageRepository;
 
-  UserService(this._userRepository, this._authRepository);
+  UserService(
+    this._userRepository,
+    this._authRepository,
+    this._imageRepository,
+  );
 
   Future<GetUserEntity> signupUserAsync(SignupFormModel signupForm) async {
     // Map model to entity
@@ -60,5 +70,40 @@ class UserService {
     UpdateUserEntity updatedUser,
   ) async {
     return await _userRepository.updateUserAsync(userId, updatedUser);
+  }
+
+  Future<void> changeUserPfpAsync(String userId, File imageFile) async {
+    final mimeType =
+        lookupMimeType(imageFile.path) ?? 'application/octet-stream';
+
+    // First request a presigned URL from the backend
+    final s3UploadInfo = await _imageRepository.requestProfileImageUpload(
+      CreateImageReqEntity(contentType: mimeType),
+    );
+
+    // Then upload the image to the presigned URL
+    final uploadSuccess = await _imageRepository.uploadProfileImage(
+      s3UploadInfo.uploadUrl,
+      imageFile,
+    );
+
+    // Extract key to use to confirm with backend
+    final key = s3UploadInfo.key;
+
+    // Finally, confirm and update the user's profile picture URL in the backend
+    if (uploadSuccess) {
+      final confirmSuccess = await _imageRepository.confirmProfileImageUpload(
+        ConfirmImageReqEntity(key: key),
+      );
+
+      if (confirmSuccess) {
+        // Update the user's profile with the new image key
+        print('Success confirmed image upload with backend');
+      } else {
+        print('Failed to confirm image upload with backend');
+      }
+    } else {
+      print('Failed to upload image to S3');
+    }
   }
 }
